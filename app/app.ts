e66ead8f5c3ff6113651a5f68649d38dbbb86047
Toni sanchez <toninos@gmail.com>
2019-09-18 16:20:02 +0100

    Adding nunjucks and govuk style library

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,21 @@
 import * as express from 'express';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+import * as nunjucks from 'nunjucks';
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+app.use(express.static('build', { maxAge: 31557600000 }));
+
 app.get('/', (req: Request, res: Response) => {
-    res.send({
-        message: 'hello world',
-    });
+    res.render('index.html', { data: 'Hello from the other side' });
 });
+
+nunjucks.configure('app/views', {
+    autoescape: true,
+    express: app
+});
+
 app.listen(PORT, () => {
     console.log('server started at http://localhost:' + PORT);
 });

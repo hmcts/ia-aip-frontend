60bcd1922d3b08291458223219eb5f00553a07e2
Toni sanchez <toninos@gmail.com>
2019-09-18 16:19:07 +0100

    Webpack config and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const nodeExternals = require('webpack-node-externals');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const path = require('path');
-const {
-  NODE_ENV = 'production',
-} = process.env;
+const { NODE_ENV = 'production' } = process.env;
+
 module.exports = {
   entry: './app/app.ts',
   mode: NODE_ENV,
   target: 'node',
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: ['yarn run:dev']
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'public' },
+      { from: 'node_modules/govuk-frontend/govuk/all.css', to: 'public' }
+    ])
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js'
@@ -15,6 +27,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  externals: [ nodeExternals() ],
   module: {
     rules: [
       {
@@ -25,6 +38,4 @@ module.exports = {
       }
     ]
   },
-  externals: [ nodeExternals() ],
-  watch: NODE_ENV === 'development'
 };

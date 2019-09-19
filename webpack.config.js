c2ebd74a8bf72ffdbcdccec91087dac4c7bdc056
Toni <toninos@gmail.com>
2019-09-19 12:31:58 +0100

    adding sass loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const nodeExternals = require('webpack-node-externals');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 const { NODE_ENV = 'production' } = process.env;
 
 module.exports = {
-  entry: './app/app.ts',
+  entry: [
+    './app/app.ts',
+    './app/assets/scss/application.scss'
+  ],
   mode: NODE_ENV,
   target: 'node',
   plugins: [
@@ -16,9 +20,11 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'public' },
-      { from: 'node_modules/govuk-frontend/govuk/all.css', to: 'public' }
-    ])
+      { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'public' }
+    ]),
+    new MiniCSSExtractPlugin({
+      filename: 'public/[name].css'
+    })
   ],
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -34,6 +40,14 @@ module.exports = {
         test: /\.ts$/,
         use: [
           'ts-loader',
+        ]
+      },
+      { 
+        test: /\.(sa|sc|c)ss$/, 
+        loader: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
         ]
       }
     ]

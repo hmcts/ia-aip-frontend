9d3ac959da18a6db40aeebff0473bcb1bdfffdc2
chrisgrimble <chris.grimble@hmcts.net>
2019-09-20 14:01:50 +0100

    Setup build

---
+++ infrastructure/aat.tfvars
@@ -0,0 +1,2 @@
+infrastructure_env  = "preprod"
+additional_hostname = "ia-api-frontend.nonprod.platform.hmcts.net"
---
+++ infrastructure/main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {}
+
+locals {
+
+  preview_app_service_plan        = "${var.product}-${var.component}-${var.env}"
+  non_preview_app_service_plan    = "${var.product}-${var.env}"
+  app_service_plan                = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
+
+  preview_vault_name              = "${var.raw_product}-aat"
+  non_preview_vault_name          = "${var.raw_product}-${var.env}"
+  key_vault_name                  = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = "${var.location}"
+  tags     = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+}
+
+data "azurerm_key_vault" "ia_key_vault" {
+  name                = "${local.key_vault_name}"
+  resource_group_name = "${local.key_vault_name}"
+}
+
+module "ia_aip_frontend" {
+  source               = "git@github.com:hmcts/cnp-module-webapp?ref=master"
+  product              = "${var.product}-${var.component}"
+  location             = "${var.location}"
+  env                  = "${var.env}"
+  ilbIp                = "${var.ilbIp}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  is_frontend          = "${var.env != "preview" ? 1: 0}"
+  subscription         = "${var.subscription}"
+  additional_host_name = "${var.env != "preview" ? var.additional_hostname : "null"}"
+  https_only           = "${var.env != "preview" ? "true" : "true"}"
+  capacity             = "${var.capacity}"
+  instance_size        = "${var.instance_size}"
+  common_tags          = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+  asp_name             = "${local.app_service_plan}"
+  asp_rg               = "${local.app_service_plan}"
+
+  app_settings = {
+    WEBSITE_NODE_DEFAULT_VERSION = "8.11.1"
+    NODE_ENV                     = "${var.infrastructure_env}"
+  }
+}

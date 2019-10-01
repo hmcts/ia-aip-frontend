13d3959346b7deb252ca18308f2b0cccaeb728a6
chrisgrimble <chris.grimble@hmcts.net>
2019-10-01 14:14:25 +0100

    Setup functional tests

---
+++ test/functional/example.test.ts
@@ -0,0 +1,44 @@
+import { IndexPage } from './page-objects/IndexPage';
+
+const puppeteer = require('puppeteer');
+import { Page, Browser } from 'puppeteer';
+const config = require('config');
+const httpProxy = config.get('httpProxy');
+
+describe('Load index page', () => {
+  let indexPage: IndexPage;
+
+  async function startBrowser() {
+    const args = ['--no-sandbox', '--start-maximized'];
+    if (httpProxy) {
+      args.push(`-proxy-server=${httpProxy}`);
+    }
+
+    const opts = {
+      args,
+      headless: true,
+      timeout: 10000,
+      ignoreHTTPSErrors: true
+    };
+    return puppeteer.launch(opts);
+  }
+
+  before('start services and bootstrap data in CCD/COH', async () => {
+    const browser: Browser = await startBrowser();
+    const page: Page = await browser.newPage();
+    indexPage = new IndexPage(page);
+    await indexPage.gotoPage();
+  });
+
+  after(async () => {
+    if (indexPage && indexPage.close) {
+      await indexPage.close();
+    }
+  });
+
+  it('load index page check title', () => {
+    return indexPage.verifyPage();
+  });
+});
+
+export {};
---
+++ test/functional/page-objects/BasePage.ts
@@ -0,0 +1,30 @@
+import { Page } from 'puppeteer';
+const { expect } = require('../config');
+const config = require('config');
+
+const testUrl = config.get('testUrl');
+
+export class BasePage {
+  public page: Page;
+  public pageTitle: String;
+  public pagePath: String;
+
+  constructor(page: Page, pageTitle: String, pagePath: String) {
+    this.page = page;
+    this.pageTitle = pageTitle;
+    this.pagePath = pagePath;
+  }
+
+  async verifyPage() {
+    const title = await this.page.title();
+    expect(title).to.equal(this.pageTitle);
+  }
+
+  async close() {
+    await this.page.close();
+  }
+
+  async gotoPage() {
+    await this.page.goto(`${testUrl}${this.pagePath}`);
+  }
+}

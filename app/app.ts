0caad8e3da6a7b8ccf1ad40501c41812f08e9c91
chrisgrimble <chris.grimble@hmcts.net>
2019-10-04 14:06:54 +0100

    Added express code to setup redis

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,45 @@
+import * as path from 'path';
+import express from 'express';
 import * as http from 'http';
 import * as nunjucks from 'nunjucks';
-
 import { router } from './routes';
-
-const path = require('path');
-import express = require('express');
+import expressSession from 'express-session';
+import * as redis from 'redis';
+import redisConnect from 'connect-redis';
+import config from 'config';
 
 const port: number | string = process.env.PORT || 3000;
 const isDev: boolean = process.env.NODE_ENV === 'development';
 
 const app: express.Application = express();
+
+const redisStore: expressSession.Store = redisConnect(expressSession);
+const redisClient = redis.createClient();
+redisClient.on('error', (err) => {
+  // tslint:disable-next-line no-console
+  console.log('Redis error: ', err);
+});
+
+const store = new redisStore({
+  url: config.get('session.redis.url'),
+  ttl: config.get('session.redis.ttlInSeconds'),
+  client: redisClient
+});
+
+const isSecure = config.get('session.cookie.secure') === 'true';
+
+app.use(expressSession({
+  cookie: {
+    httpOnly: true,
+    maxAge: config.get('session.cookie.maxAgeInMs'),
+    secure: isSecure
+  },
+  resave: true,
+  saveUninitialized: true,
+  secret: config.get('session.redis.secret'),
+  rolling: true,
+  store
+}));
 
 nunjucks.configure([
   'views',
@@ -23,13 +53,14 @@ nunjucks.configure([
 app.use(express.static('build', { maxAge: 31557600000 }));
 app.use(router);
 
-app.get('/health', (req: express.Request, res: express.Response) => {
-  res.json({ status: 'UP', version: process.version });
+app.get('/health', (req: expressSession.Request, res: expressSession.Response) => {
+  req.session.health = req.session.health ? req.session.health + 1 : 1;
+  res.json({ status: 'UP', count: req.session.health });
 });
-app.get('/liveness', (req: express.Request, res: express.Response) => {
+app.get('/liveness', (req: expressSession.Request, res: expressSession.Response) => {
   res.json({});
 });
-app.get('/health/liveness', (req: express.Request, res: express.Response) => {
+app.get('/health/liveness', (req: expressSession.Request, res: expressSession.Response) => {
   res.json({});
 });
 

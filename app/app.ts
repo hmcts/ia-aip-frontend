b178d30d260d556e5ae8c57e8d58e85728f63c42
Toni sanchez <toninos@gmail.com>
2019-10-08 16:46:24 +0100

    Adding logger and appInsights support

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
+import config from 'config';
 import * as http from 'http';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import * as nunjucks from 'nunjucks';
-import webpackDevConfig from '../webpack/webpack.dev.js';
 import { router } from './routes';
 import internationalization from './../locale/en.json';
 import { pageNotFoundHandler, serverErrorHandler } from './handlers/error-handler';
@@ -12,11 +12,20 @@ const path = require('path');
 import express = require('express');
 
 import { setupSession } from './session';
+import Logger from './utils/logger';
+import webpackDevConfig from '../webpack/webpack.dev.js';
+import {
+  logErrorMiddleware,
+  logRequestMiddleware
+} from './middleware/logger';
 
 const port: number | string = process.env.PORT || 3000;
 const app: express.Application = express();
 
 app.use(setupSession());
+const iKey = config.get('appInsights.instrumentationKey');
+const logger: Logger = new Logger(iKey);
+app.locals.logger = logger;
 
 nunjucks.configure([
   'views',
@@ -39,8 +48,10 @@ if (process.env.NODE_ENV === 'development') {
  */
 app.locals.i18n = internationalization;
 
+app.use(logRequestMiddleware);
 app.use(express.static('build', { maxAge: 31557600000 }));
 app.use(router);
+app.use(logErrorMiddleware);
 
 /**
  *  Error Handlers

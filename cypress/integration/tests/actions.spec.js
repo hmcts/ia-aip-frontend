ba9d48e4e46d1e4823495d72efb13a16d05ac300
Lewis Williams <lewis.williams@transformuk.com>
2019-10-15 10:20:27 +0100

    Added error condition check

--- cypress/integration/tests/actions.spec.js
+++ cypress/integration/tests/actions.spec.js
@@ -3,18 +3,26 @@
 context('Run e2e happy path', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+    cy.get('.testing-class');
+    cy.get('p').contains('Hello from the OTHER world!!!');
   });
 
   it('Should run e2e and check page elements.', () => {
-    cy.get('.testing-class');
-    cy.get('p').contains('Hello from the OTHER world!!!');
     cy.get('#firstName').click().type('Lewis');
     cy.get('#lastName').click().type('Williams');
     cy.get('.govuk-button').click();
-    cy.wait(3000)
+    cy.wait(3000);
     cy.url().should('include','/testPost');
     cy.get('p').contains('Lewis');
     cy.get('.govuk-back-link').click();
     cy.url().should('include','/');
+  });
+
+  it('Should run error messages', () => {
+    cy.get('#firstName').click().type('Joe');
+    cy.get('#lastName').click().type('Williams');
+    cy.get('.govuk-button').click();
+    cy.wait(3000);
+    cy.get('.govuk-error-summary').contains('Dont enter first name as \'Joe\'')
   })
 });

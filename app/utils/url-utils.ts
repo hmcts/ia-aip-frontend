705cb421dd57162a922b220162e4039b7dfa6c09
chrisgrimble <chris.grimble@hmcts.net>
2019-10-21 11:50:20 +0100

    Set redirect url based on the request url not hard coded to localhost

---
+++ app/utils/url-utils.ts
@@ -0,0 +1,13 @@
+import config from 'config';
+import { Request } from 'express';
+
+const appPort = config.get('node.port');
+
+function getUrl(protocol: string, host: string, path: string): string {
+  const portString = (host === 'localhost') ? ':' + appPort : '';
+  return protocol + '://' + host + portString + path;
+}
+
+export function getRedirectUrl(req: Request): string {
+  return getUrl(req.protocol, req.hostname, '/redirectUrl');
+}
---
+++ test/unit/utils/url-utils.test.ts
@@ -0,0 +1,28 @@
+import { Request } from 'express';
+import { getRedirectUrl } from '../../../app/utils/url-utils';
+
+import { expect } from '../../utils/testUtils';
+
+describe('creates url', () => {
+  it('not for localhost', () => {
+    const req = {
+      protocol: 'http',
+      hostname: 'example.com'
+    } as Partial<Request>;
+
+    const redirectUrl = getRedirectUrl(req as Request);
+
+    expect(redirectUrl).eq('http://example.com/redirectUrl');
+  });
+
+  it('for localhost', () => {
+    const req = {
+      protocol: 'http',
+      hostname: 'localhost'
+    } as Partial<Request>;
+
+    const redirectUrl = getRedirectUrl(req as Request);
+
+    expect(redirectUrl).eq('http://localhost:3000/redirectUrl');
+  });
+});

27d2af5d38877aa83402903d1d3af089ffa0819d
Toni sanchez <toninos@gmail.com>
2019-10-21 15:05:39 +0100

    Adding Home office reference number page

--- app/paths.ts
+++ app/paths.ts
@@ -2,7 +2,8 @@ const paths = {
   index: '/',
   health: '/health',
   liveness: '/liveness',
-  healthLiveness: '/health/liveness'
+  healthLiveness: '/health/liveness',
+  homeOfficeDetails: '/home-office-details'
 };
 
 export {

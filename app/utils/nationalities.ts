d84f81d325e8231fb4c7cfa88cef254c71f4fd73
Lewis Williams <lewis.williams@transformuk.com>
2019-10-24 14:47:10 +0100

    Refactored nationalties list

--- app/utils/nationalities.ts
+++ app/utils/nationalities.ts
@@ -1,69 +1,11 @@
-const nationalities = [
-  {
-    text: 'Please select an nationality',
-    value: ''
-  },
-  {
-    text: 'Venezuelan',
-    value: 'Venezuelan'
-  },
-  {
-    text: 'Ugandan',
-    value: 'Ugandan'
-  },
-  {
-    text: 'Romanian',
-    value: 'Romanian'
-  },
-  {
-    text: 'Mexican',
-    value: 'Mexican'
-  },
-  {
-    text: 'Canadian',
-    value: 'Canadian'
-  },
-  {
-    text: 'Dutch',
-    value: 'Dutch'
-  },
-  {
-    text: 'Lithuanian',
-    value: 'Lithuanian'
-  },
-  {
-    text: 'Estonian',
-    value: 'Estonian'
-  },
-  {
-    text: 'Finnish',
-    value: 'Finnish'
-  },
-  {
-    text: 'Spanish',
-    value: 'Spanish'
-  },
-  {
-    text: 'Polish',
-    value: 'Polish'
-  },
-  {
-    text: 'South African',
-    value: 'South African'
-  },
-  {
-    text: 'Australian',
-    value: 'Australian'
-  },
-  {
-    text: 'Indian',
-    value: 'Indiann'
-  },
-  {
-    text: 'Brazilian',
-    value: 'Brazilian'
-  }
-];
+const nations = ['Venezuelan','Ugandan','Romanian','Mexican','Dutch','Lithuanian','Estonian','Finnish','Brazilian'];
+const nationalities = [{ text: 'Please select a nationality', value: '' }];
+nations.map((nation) => {
+  nationalities.push({
+    text: nation,
+    value: nation
+  });
+});
 export {
     nationalities
 };

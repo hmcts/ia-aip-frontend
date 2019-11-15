3f52820590f2e9ea60665742d1bf8a6a5f88f53d
chrisgrimble <chris.grimble@hmcts.net>
2019-11-15 12:18:41 +0000

    Create a CCD mock
    Add a mock that can handle CCD requests. does not remember changes to
    cases so once you logout your changes will be lost. Can be used running
    yarn dev:mock then setting the CCD_API_URL to http://localhost:20000
    when running yarn dev.

---
+++ test/mock/ccd/services/loadCases.js
@@ -0,0 +1,42 @@
+const usersToCaseData = {
+  '44': [],
+  '45': [{
+    "id": 1573640323267110,
+    "jurisdiction": "IA",
+    "state": "appealStarted",
+    "version": 8,
+    "case_type_id": "Asylum",
+    "created_date": "2019-11-13T10:18:43.271",
+    "last_modified": "2019-11-13T15:35:31.356",
+    "security_classification": "PUBLIC",
+    "case_data": {
+      "journeyType": "aip",
+      "homeOfficeReferenceNumber": "A1234564"
+    },
+    "data_classification": {
+      "journeyType": "PUBLIC",
+      "homeOfficeReferenceNumber": "PUBLIC"
+    },
+    "after_submit_callback_response": null,
+    "callback_response_status_code": null,
+    "callback_response_status": null,
+    "delete_draft_response_status_code": null,
+    "delete_draft_response_status": null,
+    "security_classifications": {
+      "journeyType": "PUBLIC",
+      "homeOfficeReferenceNumber": "PUBLIC"
+    }
+  }]
+};
+
+module.exports = {
+  path: '/citizens/:userId/jurisdictions/:jurisdictionId/case-types/:caseType/cases',
+  method: 'GET',
+  template: params => {
+    console.log('Getting for user [' + params.userId +']');
+
+    const usersToCaseDatum = usersToCaseData[params.userId];
+
+    return usersToCaseDatum;
+  }
+};

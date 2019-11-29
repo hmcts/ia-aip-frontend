c8380c4a7e825c14ceddb404c0a505d6a50ebd33
Lewis Williams <lewis.williams@transformuk.com>
2019-11-29 11:31:31 +0000

    Updated isLate field

--- app/controllers/confirmation-page.ts
+++ app/controllers/confirmation-page.ts
@@ -4,7 +4,12 @@ import { paths } from '../paths';
 
 function getConfirmationPage(req: Request, res: Response, next: NextFunction) {
   try {
+      // Check session data if isLate is true then return true else return late
+    const { application } = req.session.appeal;
+    const isLate = () => application.isAppealLate === true;
 
+    // tslint:disable-next-line:no-console
+    console.log(req);
     const daysToWaitUntilContact = (days: number) => {
       const date = moment().add(days,'days').format('Do MMMM YYYY');
       return date;
@@ -12,7 +17,7 @@ function getConfirmationPage(req: Request, res: Response, next: NextFunction) {
     res.render('confirmation-page.njk', {
       date: daysToWaitUntilContact,
       days: 14,
-      late: false
+      late: isLate()
     });
   } catch (e) {
     next(e);

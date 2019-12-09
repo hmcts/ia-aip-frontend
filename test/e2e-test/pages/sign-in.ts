aef0a81ea5dbd30e21fd51d2b53bf11129190ec5
Toni sanchez <toninos@gmail.com>
2019-12-09 11:59:20 +0000

    Using typescript on functional tests

--- test/e2e-test/pages/sign-in.ts
+++ test/e2e-test/pages/sign-in.ts
@@ -1,30 +1,26 @@
-const {signIn } = require("./helper-functions");
-
+const { signInHelper } = require('./helper-functions');
 const testUrl = require('config').get('testUrl');
 
 module.exports = {
-signIn(I) {
+  signIn(I) {
     Given('I have a defined step', () => {
-        I.amOnPage(testUrl);
+      I.amOnPage(testUrl);
     });
 
     When('I click start now', async () => {
-      await  I.click('.govuk-button');
+      await I.click('.govuk-button');
     });
 
     Then('I should see the sign in page', async () => {
-        await I.seeInTitle('Sign in - HMCTS Access')
+      await I.seeInTitle('Sign in - HMCTS Access');
     });
 
     When('I enter creds and click sign in',async () => {
-        await signIn()
+      await signInHelper();
     });
 
     Then('I should see the task-list page', async () => {
-        await I.seeInCurrentUrl('task-list')
+      await I.seeInCurrentUrl('task-list');
     });
-
-}
-
-
-}
+  }
+};

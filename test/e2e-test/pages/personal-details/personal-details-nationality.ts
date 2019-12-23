e55de16044d60c143ec05e8649b84bf30b2637cb
chrisgrimble <chris.grimble@hmcts.net>
2019-12-23 15:49:08 +0000

    RIA_2220 Save for later
    Added functional test for personal details date of birth

---
+++ test/e2e-test/pages/personal-details/personal-details-nationality.ts
@@ -0,0 +1,16 @@
+import { paths } from '../../../../app/paths';
+const config = require('config');
+
+const testUrl = config.get('testUrl');
+
+module.exports = {
+  nationality(I) {
+    Given('I am on the personal details nationality page', async () => {
+      I.amOnPage(testUrl + paths.personalDetails.nationality);
+    });
+
+    Then('I should see the nationality page', async () => {
+      I.seeInCurrentUrl(paths.personalDetails.nationality);
+    });
+  }
+};

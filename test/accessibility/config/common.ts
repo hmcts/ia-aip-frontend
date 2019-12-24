c978f991f6ea33b452e6c1fd4aca40c649e5456d
chrisgrimble <chris.grimble@hmcts.net>
2019-12-24 15:15:50 +0000

    RIA-2356 Setup Pa11y
    Pa11y should check each page but add a test that takes the paths file
    and runs Pa11y on it.

--- test/accessibility/config/common.ts
+++ test/accessibility/config/common.ts
@@ -1,6 +1,8 @@
 export const pa11yConfig = {
   includeWarnings: false,
-  ignore: ['notice'],
+  ignore: ['notice', 'WCAG2AA.Principle1.Guideline1_3.1_3_1.F92,ARIA4'],
   chromeLaunchConfig: { ignoreHTTPSErrors: true },
-  hideElements: 'link[rel=mask-icon], .govuk-header__logotype-crown, .govuk-footer__licence-logo, .govuk-skip-link, .govuk-footer__link'
+  hideElements: 'link[rel=mask-icon], .govuk-header__logotype-crown, .govuk-footer__licence-logo, .govuk-skip-link, .govuk-footer__link',
+  page: undefined,
+  browser: undefined
 };

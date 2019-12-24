f7a0f3310edfeb457244b6e0c0d1df8eaa244cf2
chrisgrimble <chris.grimble@hmcts.net>
2019-12-24 10:04:44 +0000

    RIA_2220 Save for later
    Don't cache response from load case as it can change

--- test/mock/ccd/services/loadCases.js
+++ test/mock/ccd/services/loadCases.js
@@ -34,6 +34,7 @@ const usersToCaseData = {
 module.exports = {
   path: '/citizens/:userId/jurisdictions/:jurisdictionId/case-types/:caseType/cases',
   method: 'GET',
+  cache: false,
   template: params => {
     if (params.userId === '999') {
       const caseData = cache.get('caseData');

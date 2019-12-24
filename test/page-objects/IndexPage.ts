fb507c51816890711cfb39351618c7d9cb5d9d6b
chrisgrimble <chris.grimble@hmcts.net>
2019-12-24 15:15:50 +0000

    RIA-2356 Setup Pa11y
    Pa11y should check each page but add a test that takes the paths file
    and runs Pa11y on it.

--- test/page-objects/IndexPage.ts
+++ test/page-objects/IndexPage.ts
@@ -1,7 +1,22 @@
+import { expect } from '../utils/testUtils';
 import { BasePage } from './BasePage';
 
 export class IndexPage extends BasePage {
   constructor(page) {
     super(page, 'Home', '/');
+  }
+
+  async login() {
+    await Promise.all([
+      this.page.waitForNavigation(),
+      this.page.click('.govuk-button')
+    ]);
+    await Promise.all([
+      this.page.waitForNavigation(),
+      this.page.click('#login')
+    ]);
+
+    const title = await this.page.title();
+    expect(title).to.equal('Task List - Immigration & Asylum - GOV.UK');
   }
 }

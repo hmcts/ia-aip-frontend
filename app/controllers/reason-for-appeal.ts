7039ca37dff556ac6be8151c12d52f8540964339
Lewis Williams <lewis.williams@transformuk.com>
2020-01-07 15:55:28 +0000

    ria-2264 reasons upload page

--- app/controllers/reason-for-appeal.ts
+++ app/controllers/reason-for-appeal.ts
@@ -1,9 +1,9 @@
 
 import { NextFunction, Request, Response, Router } from 'express';
-import multer from 'multer';
 import { paths } from '../paths';
+import { Events } from '../service/ccd-service';
 import UpdateAppealService from '../service/update-appeal-service';
-import { homeOfficeDecisionValidation, textAreaValidation } from '../utils/fields-validations';
+import { homeOfficeDecisionValidation } from '../utils/fields-validations';
 import { daysToWaitUntilContact } from './confirmation-page';
 
 function getReasonForAppeal(req: Request, res: Response, next: NextFunction) {
@@ -51,56 +51,19 @@ function getConfirmationPage(req: Request, res: Response, next: NextFunction) {
 
 function getReasonsUploadPage(req: Request, res: Response, next: NextFunction) {
   try {
-    const evidence = req.session.appeal.caseBuilding.evidences || {};
     return res.render('case-building/reasons-for-appeal/reasons-for-appeal-upload.njk',{
-      evidences: Object.values(evidence),
-      evidenceCTA: paths.reasonsForAppeal.deleteReason,
-      previousPage: paths.reasonsForAppeal.decision
+
     });
   } catch (e) {
     next(e);
   }
 }
 
-function postDeleteEvidence(req: Request, res: Response, next: NextFunction) {
-  try {
-    if (req.body.delete) {
-      const fileId = Object.keys(req.body.delete)[0];
-      delete req.session.appeal.caseBuilding.evidences[fileId];
-    }
-    return res.redirect(paths.reasonsForAppeal.uplaod);
-  } catch (e) {
-    next(e);
-  }
-}
-
-function postUploadEvidence(req: Request, res: Response, next: NextFunction) {
-  try {
-    if (req.file) {
-      const { caseBuilding } = req.session.appeal;
-      caseBuilding.evidences = {
-        ...caseBuilding.evidences,
-        [req.file.originalname]: {
-          url: req.file.originalname,
-          name: req.file.originalname
-        }
-      };
-    }
-    return res.redirect(paths.reasonsForAppeal.uplaod);
-  } catch (e) {
-    next(e);
-  }
-}
-
-const upload = multer().single('file-upload');
-
 function setupReasonsForAppealController(deps?: any): Router {
   const router = Router();
   router.get(paths.reasonsForAppeal.decision, getReasonForAppeal);
   router.post(paths.reasonsForAppeal.decision, postReasonForAppeal(deps.updateAppealService));
   router.get(paths.reasonsForAppeal.confirmation, getConfirmationPage);
-  router.post(paths.reasonsForAppeal.deleteReason, upload, postDeleteEvidence);
-  router.post(paths.reasonsForAppeal.fileUpload, upload, postUploadEvidence);
   router.get(paths.reasonsForAppeal.uplaod, getReasonsUploadPage);
 
   return router;
@@ -111,8 +74,6 @@ export {
     getReasonForAppeal,
     postReasonForAppeal,
     getConfirmationPage,
-    getReasonsUploadPage,
-    postDeleteEvidence,
-    postUploadEvidence
+    getReasonsUploadPage
 
 };

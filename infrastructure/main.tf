470874499c596864d6d594a5621e98499b78ec59
chrisgrimble <chris.grimble@hmcts.net>
2020-01-24 16:41:10 +0000

    RIA-2414 Setup AKS
    Get redis url from vault.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -85,3 +85,15 @@ module "redis-cache" {
   subnetid    = "${data.terraform_remote_state.core_apps_infrastructure.subnet_ids[1]}"
   common_tags = "${var.common_tags}"
 }
+
+resource "azurerm_key_vault_secret" "redis_access_key" {
+  name         = "${var.product}-redis-access-key"
+  value        = "${module.redis-cache.access_key}"
+  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+}
+
+resource "azurerm_key_vault_secret" "redis_connection_string" {
+  name         = "${var.product}-redis-connection-string"
+  value        = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+}

caff7e1571fa08865d062431db4e4f73a6282f43
chrisgrimble <chris.grimble@hmcts.net>
2020-02-11 09:38:27 +0000

    RIA-2538 User journey is incorrect for user without an Idam account
    When a user is creating an account after setting up their account they
    are redirected back to our start page. Changed this to redirect to
    '/login' which will send them back to the login page of idam.

--- app/config/idam-config.ts
+++ app/config/idam-config.ts
@@ -1,10 +1,11 @@
+import { paths } from '../paths';
 import { setupSecrets } from '../setupSecrets';
 
 const config = setupSecrets();
 
 export const idamConfig = {
   redirectUri: 'https://localhost:3000/redirectUrl',
-  indexUrl: '/start',
+  indexUrl: paths.login,
   idamApiUrl: config.get('idam.apiUrl'),
   idamLoginUrl: `${config.get('idam.webUrl')}/login`,
   idamSecret: config.get('idam.secret'),

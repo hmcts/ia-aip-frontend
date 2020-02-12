4cf9b415eb6aeb45030087fbd76cba140ce18c86
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-02-12 15:13:03 +0000

    Applying suggestions from PR Extracting mock data cases into files to avoid cluttering the load cases mock endpoint.

---
+++ test/mock/ccd/mock-case-data/data/partial-appeal-started.ts
@@ -0,0 +1,13 @@
+export const partialAppealStartedCaseData = {
+  'id': 1573640323267110,
+  'jurisdiction': 'IA',
+  'state': 'appealStarted',
+  'version': 8,
+  'case_type_id': 'Asylum',
+  'created_date': '2019-11-13T10:18:43.271',
+  'last_modified': '2019-11-13T15:35:31.356',
+  'case_data': {
+    'journeyType': 'aip',
+    'homeOfficeReferenceNumber': 'A1234564'
+  }
+};

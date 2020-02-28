8d86dd6e22fa1fb874d29192dca7d1a0b47d3827
chrisgrimble <chris.grimble@hmcts.net>
2020-02-28 10:32:33 +0000

    Fix missing slash

--- test/e2e-test/pages/error-pages/404-error-page.ts
+++ test/e2e-test/pages/error-pages/404-error-page.ts
@@ -4,7 +4,7 @@ const testUrl = configFile.get('testUrl');
 module.exports = {
   error404(I) {
     Given('I enter a non existent url', async () => {
-      await I.amOnPage(testUrl + 'non-existent-url');
+      await I.amOnPage(testUrl + '/non-existent-url');
     });
 
     Then(/^I should see page not found error page$/, async () => {

9d037feb70f1ad03a660b455687a813dfc01f373
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-03-03 12:27:50 +0000

    Fixing e2e test

--- test/e2e-test/pages/overview-page/overview-page.ts
+++ test/e2e-test/pages/overview-page/overview-page.ts
@@ -29,12 +29,12 @@ module.exports = {
 
     Then(/^I should see the 'do this next section' for 'Awaiting reasons for appeal'$/, () => {
       I.see(i18n.pages.overviewPage.doThisNext.toDo, '//*[@id="doThisNextTitle"]/h2[1]');
-      I.seeInSource(`<p class="govuk-body">${i18n.pages.overviewPage.doThisNext.awaitingReasonsForAppeal.tellUsWhy}</p>`);
+      I.seeInSource(`<p class="govuk-body">${i18n.pages.overviewPage.doThisNext.awaitingReasonsForAppeal.new.description}</p>`);
     });
 
     Then(/^I should see the 'do this next section' for 'Saved - Awaiting reasons for appeal'$/, () => {
       I.see(i18n.pages.overviewPage.doThisNext.toDo, '//*[@id="doThisNextTitle"]/h2[1]');
-      I.seeInSource(`<p class="govuk-body">${i18n.pages.overviewPage.doThisNext.awaitingReasonsForAppeal.partiallySaved}</p>`);
+      I.seeInSource(`<p class="govuk-body">${i18n.pages.overviewPage.doThisNext.awaitingReasonsForAppeal.partial.description}</p>`);
     });
   }
 };

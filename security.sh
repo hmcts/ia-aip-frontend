b57c27d9ffc6c763e12f34b4a4004457ec228147
chrisgrimble <chris.grimble@hmcts.net>
2020-03-04 15:41:05 +0000

    Ignore some zap errors

--- security.sh
+++ security.sh
@@ -30,9 +30,9 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   cp *.html functional-output/
   cp activescanReport.xml functional-output/
 
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.* functional-output/
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
 
 echo
 echo ZAP Security vulnerabilities were found that were not ignored

87574cb967e490a83941f7e341b64fe2d5acaadc
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2020-03-11 10:52:55 +0000

    Linked guidance pages. (#240)
    * RIA-2490 
    - Change description
    - Linked guidance pages.
    - Changed behaviour of back button.
    - Added tests to cover the links and back button.
    - Updated existing tests

--- app/controllers/guidance-page.ts
+++ app/controllers/guidance-page.ts
@@ -5,10 +5,13 @@ import { getGuidancePageText } from '../utils/guidance-page-utils';
 function getCaseworkerPage(req: Request, res: Response, next: NextFunction) {
   const text = getGuidancePageText('caseworker');
   try {
-    return res.render('guidance-pages/guidance-page.njk',{
+    return res.render('guidance-pages/guidance-page.njk', {
       showContactUs: true,
-      previousPage: '/',
-      page: text});
+      previousPage: {
+        attributes: { onclick: 'history.go(-1); return false;' }
+      },
+      page: text
+    });
   } catch (e) {
     next(e);
   }
@@ -17,10 +20,13 @@ function getCaseworkerPage(req: Request, res: Response, next: NextFunction) {
 function getMoreHelpPage(req: Request, res: Response, next: NextFunction) {
   const text = getGuidancePageText('helpWithAppeal');
   try {
-    return res.render('guidance-pages/guidance-page.njk',{
+    return res.render('guidance-pages/guidance-page.njk', {
       showContactUs: true,
-      previousPage: '/',
-      page: text});
+      previousPage: {
+        attributes: { onclick: 'history.go(-1); return false;' }
+      },
+      page: text
+    });
   } catch (e) {
     next(e);
   }
@@ -29,10 +35,13 @@ function getMoreHelpPage(req: Request, res: Response, next: NextFunction) {
 function getHomeOfficeDocumentsPage(req: Request, res: Response, next: NextFunction) {
   const text = getGuidancePageText('homeOfficeDocuments');
   try {
-    return res.render('guidance-pages/guidance-page.njk',{
+    return res.render('guidance-pages/guidance-page.njk', {
       showContactUs: false,
-      previousPage: '/',
-      page: text});
+      previousPage: {
+        attributes: { onclick: 'history.go(-1); return false;' }
+      },
+      page: text
+    });
   } catch (e) {
     next(e);
   }
@@ -41,10 +50,13 @@ function getHomeOfficeDocumentsPage(req: Request, res: Response, next: NextFunct
 function getEvidenceToSupportAppealPage(req: Request, res: Response, next: NextFunction) {
   const text = getGuidancePageText('evidenceToSupportAppeal');
   try {
-    return res.render('guidance-pages/guidance-page.njk',{
+    return res.render('guidance-pages/guidance-page.njk', {
       showContactUs: true,
-      previousPage: '/',
-      page: text});
+      previousPage: {
+        attributes: { onclick: 'history.go(-1); return false;' }
+      },
+      page: text
+    });
   } catch (e) {
     next(e);
   }
@@ -52,11 +64,10 @@ function getEvidenceToSupportAppealPage(req: Request, res: Response, next: NextF
 
 function setupGuidancePagesController(): Router {
   const router = Router();
-  // TODO Add all pages to path and functions
-  router.get(paths.tribunalCaseworker, getCaseworkerPage);
-  router.get(paths.moreHelp, getMoreHelpPage);
-  router.get(paths.homeOfficeDocuments, getHomeOfficeDocumentsPage);
-  router.get(paths.evidenceToSupportAppeal, getEvidenceToSupportAppealPage);
+  router.get(paths.guidancePages.tribunalCaseworker, getCaseworkerPage);
+  router.get(paths.guidancePages.moreHelp, getMoreHelpPage);
+  router.get(paths.guidancePages.homeOfficeDocuments, getHomeOfficeDocumentsPage);
+  router.get(paths.guidancePages.evidenceToSupportAppeal, getEvidenceToSupportAppealPage);
   return router;
 }
 

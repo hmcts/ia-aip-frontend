4fa1e4d5c4c3364e4f341371a351cd7f6404d9c5
chrisgrimble <chris.grimble@hmcts.net>
2020-04-17 12:29:15 +0100

    Quick and dirty way to setup a user who has submitted an appeal and the TCW has requested their reasons for appeal

---
+++ codecept.setup.js
@@ -0,0 +1,27 @@
+const config = require('config')
+
+exports.config = {
+  name: 'codecept',
+  output: './functional-output/e2e/reports/',
+  helpers: {
+    Puppeteer: {
+      url: config.get('testUrl'),
+      show: config.get('showTests'),
+      "chrome": {
+        "ignoreHTTPSErrors": true
+      }
+    }
+  },
+  gherkin: {
+    features: './test/e2e-test/setup/reasons_for_appeal_requested.feature',
+    steps: ['./test/e2e-test/step_definitions/steps.ts']
+  },
+  plugins: {
+    stepByStepReport: {
+      enabled: true,
+      fullPageScreenshots: true,
+      deleteSuccessful: false
+    }
+  },
+  require: ['ts-node/register/transpile-only']
+};

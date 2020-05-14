65ac57643f358a95877d99ee20353a574fe9d104
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2020-05-14 10:01:33 +0100

    Update status-update-helper.js

--- test/crossbrowser/helpers/status-update-helper.js
+++ test/crossbrowser/helpers/status-update-helper.js
@@ -8,6 +8,7 @@ function updateResult(result, sessionId, config) {
   credentials = credentials.concat(":");
   credentials = credentials.concat(config.key);
   
+  // tslint:disable:no-console
   console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=ia-aip-frontend');
   return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + credentials + ' https://eu-central-1.saucelabs.com/rest/v1/' + username + '/jobs/' + sessionId;
 }

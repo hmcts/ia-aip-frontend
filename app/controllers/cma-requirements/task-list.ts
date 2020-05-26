6a9dae5e81a250ebfb8bd934fe5445e10143902f
Toni sanchez <toninos@gmail.com>
2020-05-26 11:24:11 +0100

    Refactoring submit event

--- app/controllers/cma-requirements/task-list.ts
+++ app/controllers/cma-requirements/task-list.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import { cmaRequirementsStatusUpdate } from '../../middleware/session-middleware';
 import { paths } from '../../paths';
+import { cmaRequirementsStatus } from '../../utils/tasks-utils';
 
 /**
  * Creates a new Section object and determines the current status of the step using the taskIds provided.
@@ -8,19 +8,19 @@ import { paths } from '../../paths';
  * @param taskIds the taskId under the section used to check for saved status and completion status
  * @param req the request Object containing the session
  */
-function buildSectionObject(sectionId: string, taskIds: string[], req: Request) {
+function buildSectionObject(sectionId: string, taskIds: string[], status: ApplicationStatus) {
   const tasks: Task[] = [];
 
   function isSaved(taskId: string) {
-    return req.session.appeal.cmaRequirements.tasks[taskId].saved;
+    return status[taskId].saved;
   }
 
   function isCompleted(taskId: string) {
-    return req.session.appeal.cmaRequirements.tasks[taskId].completed;
+    return status[taskId].completed;
   }
 
   function isActive(taskId: string) {
-    return req.session.appeal.cmaRequirements.tasks[taskId].active;
+    return status[taskId].active;
   }
 
   taskIds.forEach((taskId) => {
@@ -35,11 +35,11 @@ function buildSectionObject(sectionId: string, taskIds: string[], req: Request) 
   return section;
 }
 
-function getCmaRequirementsStatus(session: Request) {
-  const accessNeeds = buildSectionObject('accessNeeds', [ 'accessNeeds' ], session);
-  const otherNeeds = buildSectionObject('otherNeeds', [ 'otherNeeds' ], session);
-  const datesToAvoid = buildSectionObject('datesToAvoid', [ 'datesToAvoid' ], session);
-  const checkAndSend = buildSectionObject('checkAndSend', [ 'checkAndSend' ], session);
+function getCmaRequirementsStatus(status: ApplicationStatus) {
+  const accessNeeds = buildSectionObject('accessNeeds', [ 'accessNeeds' ], status);
+  const otherNeeds = buildSectionObject('otherNeeds', [ 'otherNeeds' ], status);
+  const datesToAvoid = buildSectionObject('datesToAvoid', [ 'datesToAvoid' ], status);
+  const checkAndSend = buildSectionObject('checkAndSend', [ 'checkAndSend' ], status);
 
   return [
     accessNeeds,
@@ -50,9 +50,9 @@ function getCmaRequirementsStatus(session: Request) {
 }
 
 function getTaskList(req: Request, res: Response, next: NextFunction) {
-
   try {
-    const statusOverview = getCmaRequirementsStatus(req);
+    const status: ApplicationStatus = cmaRequirementsStatus(req.session.appeal);
+    const statusOverview = getCmaRequirementsStatus(status);
 
     return res.render('cma-requirements/task-list.njk', {
       previousPage: paths.common.overview,
@@ -65,7 +65,7 @@ function getTaskList(req: Request, res: Response, next: NextFunction) {
 
 function setupCmaRequirementsTaskListController(middleware: Middleware[]) {
   const router = Router();
-  router.get(paths.awaitingCmaRequirements.taskList, middleware, cmaRequirementsStatusUpdate, getTaskList);
+  router.get(paths.awaitingCmaRequirements.taskList, middleware, getTaskList);
   return router;
 }
 

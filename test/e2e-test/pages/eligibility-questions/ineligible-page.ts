685ad4f78f39dd72a1a745a6dc6f39a28ea7ee68
Gavin Lilley <g@gavinlilley.com>
2021-04-22 14:32:26 +0100

    RIA-4285 remove deprecated messages

--- test/e2e-test/pages/eligibility-questions/ineligible-page.ts
+++ test/e2e-test/pages/eligibility-questions/ineligible-page.ts
@@ -3,7 +3,7 @@ import i18n from '../../../../locale/en.json';
 module.exports = {
   ineligibile(I) {
     Then('I should see the ineligible page', async () => {
-      I.seeInSource(i18n.pages.ineligiblePage.title);
+      I.seeInSource(i18n.ineligible[0].title);
     });
   }
 };

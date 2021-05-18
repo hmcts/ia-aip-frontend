7cc64c2bae5994d908b92573f68a01df8a01530d
Gavin Lilley <g@gavinlilley.com>
2021-05-18 11:18:54 +0100

    RIA-4382 diagnose flaky test

--- test/e2e-test/pages/eligibility-questions/ineligible-page.ts
+++ test/e2e-test/pages/eligibility-questions/ineligible-page.ts
@@ -3,7 +3,7 @@ import i18n from '../../../../locale/en.json';
 module.exports = {
   ineligibile(I) {
     Then('I should see the ineligible page', async () => {
-      I.seeInSource(i18n.ineligible[0].title);
+      I.see(i18n.ineligible[0].title, 'h1');
     });
   }
 };

c3785881a80021355f41943e9d131cbaa75e4de4
Gavin Lilley <g@gavinlilley.com>
2021-06-10 11:49:22 +0100

    RIA-4358 update app state unit tests

--- test/unit/utils/application-state-utils.test.ts
+++ test/unit/utils/application-state-utils.test.ts
@@ -123,7 +123,7 @@ describe('application-state-utils', () => {
         deadline: '07 March 2020',
         descriptionParagraphs: [
           'Your appeal details have been sent to the Tribunal.',
-          'A Tribunal Caseworker will contact you to tell you what to do next. This should be by <span class=\'govuk-body govuk-!-font-weight-bold\'>{{ applicationNextStep.deadline }}</span> but it might take longer than that.'
+          'A Tribunal Caseworker will contact you to tell you what happens next. This should be by <span class=\'govuk-body govuk-!-font-weight-bold\'>{{ applicationNextStep.deadline }}</span> but it might take longer than that.'
         ],
         info: {
           title: 'Helpful Information',

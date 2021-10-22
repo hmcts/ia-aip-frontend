9ae5a2361a8791094c146125aceee67dbfa811b7
Gavin Lilley <g@gavinlilley.com>
2021-10-22 10:38:16 +0100

    RIA-4835 full revert 😭

--- app/setupSecrets.ts
+++ app/setupSecrets.ts
@@ -15,7 +15,6 @@ const setupSecrets = () => {
   setSecret('secrets.ia.idam-secret', 'idam.secret');
   setSecret('secrets.ia.addressLookupToken', 'addressLookup.token');
   setSecret('secrets.ia.s2s-secret', 's2s.secret');
-  setSecret('secrets.ia.launch-darkly-sdk-key', 'launchDarkly.sdkKey');
   setSecret('secrets.ia.ia-redis-connection-string', 'session.redis.url');
   setSecret('secrets.ia.ia-redis-access-key', 'session.redis.secret');
   setSecret('secrets.ia.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');

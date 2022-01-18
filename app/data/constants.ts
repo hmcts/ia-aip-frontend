eb9a5250804c77fb67c0eda0090c3b361af84364
srinivasuluputcha <srinivasulu.putcha@hmcts.net>
2022-01-18 22:52:22 +0000

    RIA-5340 Moved the feature flags to constants

--- app/data/constants.ts
+++ app/data/constants.ts
@@ -2,5 +2,8 @@ export const CQ_NOTHING_ELSE: string = 'Nothing else';
 
 export const FEATURE_FLAGS = {
   CARD_PAYMENTS: 'online-card-payments-feature',
-  PCQ: 'pcq-feature'
+  PCQ: 'pcq-feature',
+  HEARING_REQUIREMENTS: 'aip-hearing-requirements-feature',
+  HEARING_BUNDLE: 'aip-hearing-bundle-feature',
+  OUT_OF_COUNTRY: 'aip-ooc-feature'
 };

b7ae35a4b2932bf14d7a2ef7ccfce792243f0122
srinivasuluputcha <srinivasulu.putcha@hmcts.net>
2022-01-18 22:52:22 +0000

    RIA-5340 Moved the feature flags to constants

--- app/middleware/outofcountry-feature-middleware.ts
+++ app/middleware/outofcountry-feature-middleware.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { FEATURE_FLAGS } from '../data/constants';
 import { paths } from '../paths';
 import LaunchDarklyService from '../service/launchDarkly-service';
 
 async function outOfCountryFeatureMiddleware(req: Request, res: Response, next: NextFunction) {
-  const outOfCountryFeatureEnabled: boolean = await LaunchDarklyService.getInstance().getVariation(req, 'aip-ooc-feature', false);
+  const outOfCountryFeatureEnabled: boolean = await LaunchDarklyService.getInstance().getVariation(req, FEATURE_FLAGS.OUT_OF_COUNTRY, false);
   req.app.locals.logger.trace(`Out of Country Feature Flag =  ${JSON.stringify(outOfCountryFeatureEnabled)}`, 'Out of COuntry Feature Flag');
   if (outOfCountryFeatureEnabled) {
     return next();

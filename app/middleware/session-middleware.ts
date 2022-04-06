7da38b300ba3e11c5d65f3a8619112ee8b56f131
Gregorio <greg.massara@gmail.com>
2022-04-06 16:41:15 +0100

    RIA-5671 remove redundant classes and methods

--- app/middleware/session-middleware.ts
+++ app/middleware/session-middleware.ts
@@ -23,10 +23,8 @@ async function initSession(req: Request, res: Response, next: NextFunction) {
 function checkSession(args: any = {}) {
   return (req: Request, res: Response, next: NextFunction) => {
     const tokenCookieName = args.tokenCookieName || '__auth-token';
-    const oauth2CookieName = args.tokenCookieName || '_oauth2_proxy';
-    if (req.cookies && req.cookies[tokenCookieName] && oauth2CookieName && !_.has(req, 'session.appeal.application')) {
+    if (req.cookies && req.cookies[tokenCookieName] && !_.has(req, 'session.appeal.application')) {
       res.clearCookie(tokenCookieName, '/');
-      res.clearCookie(oauth2CookieName, '/');
       res.redirect(paths.common.login);
     } else {
       next();

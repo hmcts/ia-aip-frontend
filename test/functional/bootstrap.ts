189781926c9287c017c91e7eb3463a9bbb362d93
Gregorio <greg.massara@gmail.com>
2022-06-15 14:57:46 +0100

    refactor bootstrap kill process

--- test/functional/bootstrap.ts
+++ test/functional/bootstrap.ts
@@ -105,9 +105,9 @@ async function teardown(done) {
   } finally {
     done();
     // tslint:disable-next-line:no-console
-    console.log('closing process' + process.pid);
+    console.log('closing process ' + process.pid);
     process.exitCode = 1;
-    process.kill(1);
+    process.kill(process.pid);
   }
 }
 

97420ccb1228cb19598b6567bee0320c096fc665
Gregorio <greg.massara@gmail.com>
2022-06-22 13:06:22 +0100

    RIA-5880 update yarn-audit-known-issues, update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,7 +20,7 @@ const serverConfig = {
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         use: [
           'ts-loader',
         ]
@@ -46,7 +46,7 @@ const clientConfig = {
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         exclude: /node_modules/,
         use: [
           {
@@ -81,7 +81,9 @@ const clientConfig = {
   ],
 };
 
-module.exports = {
+const commonConfig = {
   server: serverConfig,
   client: clientConfig
 };
+
+module.exports = commonConfig;

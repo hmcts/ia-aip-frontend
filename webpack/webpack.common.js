892c11887d0457fb3aa99ea22e472f4e913bbec6
Gregorio <greg.massara@gmail.com>
2022-06-23 17:28:58 +0100

    update refactor webpack to version 5

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,88 +3,84 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
-const merge = require("webpack-merge");
 
-const serverConfig = {
-  entry: [
-    './app/server.ts'
-  ],
-  target: 'node',
-  output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  externals: [ nodeExternals() ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          'ts-loader',
-        ]
-      }
-    ]
-  },
-};
+module.exports = [
+    {
+        name: "server",
+        entry: [
+            './app/server.ts'
+        ],
+        target: 'node',
+        output: {
+            path: path.resolve(__dirname, '../build'),
+            filename: '[name].js'
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        externals: [nodeExternals()],
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        'ts-loader',
+                    ]
+                }
+            ]
+        },
+    },
+    {
+        name: "client",
+        entry: [
+            './client/main.ts',
+            './app/assets/scss/application.scss'
+        ],
+        target: 'web',
+        output: {
+            path: path.resolve(__dirname, '../build'),
+            filename: 'all.js'
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        },
+                        {
+                            loader: 'ts-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        MiniCSSExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CopyWebpackPlugin([
+                {from: path.resolve('node_modules/govuk-frontend/govuk/assets/'), to: 'assets'},
+                {from: path.resolve('app/assets/images/'), to: 'assets/images'}
+            ]),
+            new MiniCSSExtractPlugin({
+                filename: '[name].css'
+            })
+        ],
+    }
+];
 
-const clientConfig = {
-  entry: [
-    './client/main.ts',
-    './app/assets/scss/application.scss'
-  ],
-  watch: true,
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: 'all.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          },
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        loader: [
-          MiniCSSExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: path.resolve('node_modules/govuk-frontend/govuk/assets/'), to: 'assets' },
-      { from: path.resolve('app/assets/images/'), to: 'assets/images' }
-    ]),
-    new MiniCSSExtractPlugin({
-      filename: '[name].css'
-    })
-  ],
-};
 
-const commonConfig = {
-  server: serverConfig,
-  client: clientConfig
-};
-
-module.exports = [commonConfig]
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,25 +1,36 @@
-const merge = require('webpack-merge');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const commonConfig = require('./webpack.common');
+const Configuration = require('webpack');
+const merge = require('webpack-merge');
 
 const serverConfig = {
-  mode: "production"
+    dependencies: ['server'],
+    mode: "production"
 };
 
 const clientConfig = {
-  mode: "production",
-  optimization: {
-    minimizer: [
-      new TerserJSPlugin({}),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  }
+    dependencies: ['client'],
+    mode: "production",
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({}),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    }
 };
 
-const devConfig = {
-  server: serverConfig,
-  client: clientConfig
+const prodConfig = {
+    serverConfig,
+    clientConfig
 };
 
-module.exports = merge.merge([commonConfig, devConfig]);
+console.debug('PROD')
+console.debug(JSON.stringify(prodConfig))
+console.debug('COMMON')
+console.debug(JSON.stringify(commonConfig))
+
+const config = merge<Configuration>(commonConfig, prodConfig);
+
+module.exports = config;
+

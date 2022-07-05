a1756b6cc23c6396e9936ec2daa53e856650bb29
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2022-07-05 17:24:43 +0100

    RIA-5821: Implement 'Ask to change something about your hearing' page (#770)
    * RIA-5821: Implement 'Ask to change something about your hearing' page
    
    * RIA-5821: Update to include application types

--- app/middleware/journeyAllowed-middleware.ts
+++ app/middleware/journeyAllowed-middleware.ts
@@ -22,9 +22,20 @@ const isJourneyAllowedMiddleware = (req: Request, res: Response, next: NextFunct
       return path.replace(new RegExp(`:${matches[1]}`), `${req.params[matches[1]]}`);
     }
   });
+
+  const makeApplicationPaths = Object.values({ ...paths.makeApplication }).map((path: string) => {
+    if (Object.keys(req.params).length === 0) return path;
+    const matches = path.match(/\/:([^\/]+)\/?$/);
+    if (!matches) return path;
+    if (matches[1] && req.params[matches[1]]) {
+      return path.replace(new RegExp(`:${matches[1]}`), `${req.params[matches[1]]}`);
+    }
+  });
+
   const allowedPaths = [
     ...appealStatusPaths,
-    ...commonPaths
+    ...commonPaths,
+    ...makeApplicationPaths
   ];
   const allowed: boolean = allowedPaths.includes(currentPath) ||
     currentPath.startsWith(paths.common.documentViewer);

0831b2be4d64e601ddfebda292f69e9c07edc085
markbrooks725 <107925801+markbrooks725@users.noreply.github.com>
2022-07-14 10:39:17 +0100

    RIA-5824 Make Application - 'Ask to change the location of the hearing'  (#778)
    * RIA-5824 Make Application - Appellant make hearing orientated application: 'Ask to change the location of the hearing' application process
    
    * RIA-5824 Fixed validation message
    
    * RIA-5824 Fixed paths and added unit tests
    
    * RIA-5824 Fixed path and added functional test
    
    * RIA-5824 Fixed path validation
    
    * RIA-5824 Added missing route and update functional test
    
    * RIA-5824 Updated unit test
    
    * RIA-5824 Fix lint issues
    
    * RIA-5824 Fix 'file-upload' validation issue
    
    Co-authored-by: Augustine Onubeze <ikechukwu.onubeze@gmail.com>

--- app/controllers/make-application/hearing-requests/setup-hearing-application-controllers.ts
+++ app/controllers/make-application/hearing-requests/setup-hearing-application-controllers.ts
@@ -1,21 +1,21 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { handleFileUploadErrors, uploadConfiguration } from '../../../middleware/file-upload-validation-middleware';
 import { paths } from '../../../paths';
 import { DocumentManagementService } from '../../../service/document-management-service';
 import UpdateAppealService from '../../../service/update-appeal-service';
 import { deleteSupportingEvidence, getProvideSupportingEvidence, getProvideSupportingEvidenceCheckAndSend, getProvideSupportingEvidenceYesOrNo, getRequestSent, postProvideSupportingEvidence, postProvideSupportingEvidenceCheckAndSend, postProvideSupportingEvidenceYesOrNo, uploadSupportingEvidence } from '../make-application-common';
+import { getPath } from '../make-application-controllers-helper';
 import { getAdjournHearingApplication, postAdjournHearingApplication } from './adjourn-hearing-application';
 import { getExpediteHearingApplication, postExpediteHearingApplication } from './expedite-hearing-application';
 import { getHearingApplicationType, postHearingApplicationType } from './hearing-application-type';
+import { getTransferHearingApplication, postTransferHearingApplication } from './transfer-hearing-application';
 
-function validate(redirectToUrl: string) {
+function validate(pathPrefix: string) {
   return (_req: Request, res: Response, next: NextFunction) => {
     try {
-      let errorCode: string;
       if (res.locals.errorCode) {
-        errorCode = res.locals.errorCode;
-      }
-      if (errorCode) {
-        return res.redirect(`${redirectToUrl}?error=${errorCode}`);
+        let redirectUrl = getPath(pathPrefix, _req.session.appeal.makeAnApplicationTypes.value.code);
+        return res.redirect(`${redirectUrl}?error=${res.locals.errorCode}`);
       }
       next();
     } catch (e) {
@@ -26,27 +26,35 @@ function validate(redirectToUrl: string) {
 
 function setupHearingApplicationControllers(middleware: Middleware[], updateAppealService: UpdateAppealService, documentManagementService: DocumentManagementService): Router {
   const router = Router();
+  router.get(paths.makeApplication.askChangeHearing, middleware, getHearingApplicationType);
   router.get(paths.makeApplication.expedite, middleware, getExpediteHearingApplication);
   router.get(paths.makeApplication.adjourn, middleware, getAdjournHearingApplication);
+  router.get(paths.makeApplication.transfer, middleware, getTransferHearingApplication);
   router.get(paths.makeApplication.supportingEvidenceExpedite, middleware, getProvideSupportingEvidenceYesOrNo);
   router.get(paths.makeApplication.supportingEvidenceAdjourn, middleware, getProvideSupportingEvidenceYesOrNo);
-  router.get(paths.makeApplication.askChangeHearing, middleware, getHearingApplicationType);
+  router.get(paths.makeApplication.supportingEvidenceTransfer, middleware, getProvideSupportingEvidenceYesOrNo);
   router.get(paths.makeApplication.provideSupportingEvidenceExpedite, middleware, getProvideSupportingEvidence);
   router.get(paths.makeApplication.provideSupportingEvidenceAdjourn, middleware, getProvideSupportingEvidence);
+  router.get(paths.makeApplication.provideSupportingEvidenceTransfer, middleware, getProvideSupportingEvidence);
   router.get(paths.makeApplication.provideSupportingEvidenceDeleteFile, middleware, deleteSupportingEvidence(documentManagementService));
   router.get(paths.makeApplication.checkAnswerExpedite, middleware, getProvideSupportingEvidenceCheckAndSend);
   router.get(paths.makeApplication.checkAnswerAdjourn, middleware, getProvideSupportingEvidenceCheckAndSend);
+  router.get(paths.makeApplication.checkAnswerTransfer, middleware, getProvideSupportingEvidenceCheckAndSend);
   router.get(paths.makeApplication.requestSent, middleware, getRequestSent);
   router.post(paths.makeApplication.expedite, middleware, postExpediteHearingApplication);
   router.post(paths.makeApplication.adjourn, middleware, postAdjournHearingApplication);
+  router.post(paths.makeApplication.transfer, middleware, postTransferHearingApplication);
   router.post(paths.makeApplication.askChangeHearing, middleware, postHearingApplicationType);
   router.post(paths.makeApplication.provideSupportingEvidenceExpedite, middleware, postProvideSupportingEvidence);
   router.post(paths.makeApplication.provideSupportingEvidenceAdjourn, middleware, postProvideSupportingEvidence);
+  router.post(paths.makeApplication.provideSupportingEvidenceTransfer, middleware, postProvideSupportingEvidence);
   router.post(paths.makeApplication.supportingEvidenceExpedite, middleware, postProvideSupportingEvidenceYesOrNo);
   router.post(paths.makeApplication.supportingEvidenceAdjourn, middleware, postProvideSupportingEvidenceYesOrNo);
-  router.post(paths.makeApplication.provideSupportingEvidenceUploadFile, middleware, validate(paths.makeApplication.provideSupportingEvidenceExpedite), uploadSupportingEvidence(documentManagementService));
-  router.post(paths.makeApplication.checkAnswerExpedite, middleware, validate(paths.makeApplication.provideSupportingEvidenceExpedite), postProvideSupportingEvidenceCheckAndSend(updateAppealService));
-  router.post(paths.makeApplication.checkAnswerAdjourn, middleware, validate(paths.makeApplication.provideSupportingEvidenceAdjourn), postProvideSupportingEvidenceCheckAndSend(updateAppealService));
+  router.post(paths.makeApplication.supportingEvidenceTransfer, middleware, postProvideSupportingEvidenceYesOrNo);
+  router.post(paths.makeApplication.provideSupportingEvidenceUploadFile, middleware, validate('provideSupportingEvidence'), uploadSupportingEvidence(documentManagementService));
+  router.post(paths.makeApplication.checkAnswerExpedite, middleware, validate('provideSupportingEvidence'), postProvideSupportingEvidenceCheckAndSend(updateAppealService));
+  router.post(paths.makeApplication.checkAnswerAdjourn, middleware, validate('provideSupportingEvidence'), postProvideSupportingEvidenceCheckAndSend(updateAppealService));
+  router.post(paths.makeApplication.checkAnswerTransfer, middleware, validate('provideSupportingEvidence'), postProvideSupportingEvidenceCheckAndSend(updateAppealService));
   return router;
 }
 

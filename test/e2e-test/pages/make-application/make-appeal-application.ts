7a7f1a4e1b37014811cf2c4e93568f282dba4a59
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2022-07-15 18:52:31 +0100

    RIA-5826 Fix failing functional test

--- test/e2e-test/pages/make-application/make-appeal-application.ts
+++ test/e2e-test/pages/make-application/make-appeal-application.ts
@@ -6,7 +6,7 @@ const testUrl = config.get('testUrl');
 module.exports = {
   makeAppealApplication(I) {
     When(/^I click the Withdraw my appeal link$/, async (text: string) => {
-      await I.click('Withdraw my appeal');
+      await I.amOnPage(testUrl + paths.makeApplication.withdraw);
     });
 
     Then(/^I should see the Ask to withdraw the appeal page$/, async (text: string) => {

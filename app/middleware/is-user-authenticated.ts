0b47c7b5a51e773384db5a9ba5552b2d024bea95
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2022-07-28 10:06:08 +0100

    RIA-5835: Make Application - Request judge review updates to notifica… (#798)
    * RIA-5835: Make Application - Request judge review updates to notifications
    
    * BAU Fix nightly Jenkins build ia-aip-frontend

--- app/middleware/is-user-authenticated.ts
+++ app/middleware/is-user-authenticated.ts
@@ -1,5 +1,14 @@
+import { paths } from '../paths';
+
+const redirectToPaths = [
+  paths.makeApplication.judgesReview
+];
+
 function isUserAuthenticated(req, res, next) {
   res.locals.authenticated = !!(req.cookies && req.cookies['__auth-token']);
+  if (!res.locals.authenticated && redirectToPaths.includes(req.originalUrl)) {
+    req.session.redirectUrl = req.originalUrl;
+  }
   next();
 }
 

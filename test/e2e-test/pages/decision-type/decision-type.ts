2d5d255d3baed3d5b4243125f3600b826251e031
nilay <nilay.patel@hmcts.net>
2022-11-22 18:30:26 +0000

    nightly functional fix

--- test/e2e-test/pages/decision-type/decision-type.ts
+++ test/e2e-test/pages/decision-type/decision-type.ts
@@ -6,7 +6,7 @@ const testUrl = config.get('testUrl');
 module.exports = {
   decisionType(I) {
     Given('I should be taken to the decision type page', async () => {
-      I.amOnPage(testUrl + paths.appealStarted.decisionType);
+      await I.seeInCurrentUrl(paths.appealStarted.decisionType);
     });
 
     When(/^I click on the decision-type link$/, async () => {
@@ -14,7 +14,7 @@ module.exports = {
     });
 
     When(/^I click on Decision with hearing as my type of decision and click Save and continue$/, async () => {
-      await I.checkOption('#decisionWithHearing');
+      await I.checkOption('decisionWithHearing');
       await I.click('Save and continue');
     });
 
@@ -22,7 +22,7 @@ module.exports = {
       I.amOnPage(testUrl + paths.appealStarted.decisionType);
     });
     When('I select I want the appeal to be decided with a hearing', async () => {
-      await I.checkOption('#answer');
+      await I.checkOption('decisionWithHearing');
       await I.click('Continue');
     });
     Then('I should see the the decision type page', async () => {
@@ -33,24 +33,25 @@ module.exports = {
       I.amOnPage(testUrl + paths.appealStarted.payNow);
     });
     When('I select No, I will pay later', async () => {
-      I.checkOption('#answer-2');
+      I.checkOption('decisionWithoutHearing');
     });
     Then('I should see the appeal payment page', async () => {
       I.seeInCurrentUrl(paths.appealStarted.payNow);
     });
+
     When(/^I select a decision (with|without) a hearing for a (non PA|PA pay now|PA pay later) appeal$/, async (hearingChoice, paChoice) => {
       if (hearingChoice === 'with') {
-        await I.checkOption('#answer');
+        await I.checkOption('decisionWithHearing');
       } else {
-        await I.checkOption('#answer-2');
+        await I.checkOption('decisionWithoutHearing');
       }
       if (paChoice !== 'non PA') {
         await I.click('Save and continue');
         await I.seeInCurrentUrl(paths.appealStarted.payNow);
         if (paChoice === 'PA pay now') {
-          await I.checkOption('#answer');
+          await I.checkOption('payNow');
         } else {
-          await I.checkOption('#answer-2');
+          await I.checkOption('payLater');
         }
       }
     });

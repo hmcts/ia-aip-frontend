ff58fb97c7a86c218312753a506646c7f3da239d
nilay <nilay.patel@hmcts.net>
2022-11-28 09:21:34 +0000

    fixing e2e

--- test/e2e-test/pages/clarifying-questions/clarifying-questions.ts
+++ test/e2e-test/pages/clarifying-questions/clarifying-questions.ts
@@ -1,7 +1,7 @@
 module.exports = {
   clarifyingQuestions(I) {
     Then(/^I see clarifying question "([^"]*)" saved$/, async (selector: string) => {
-      await I.see('SAVED',`//ol/li[1]/ul/li[${selector}]/strong`);
+      await I.see('SAVED',`//ol/li[${selector}]/ul/li/strong`);
     });
 
     Then(/^I see anything else clarifying question saved$/, async (selector: string) => {

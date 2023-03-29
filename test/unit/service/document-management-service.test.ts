44f19ea888d9961bc7cead8e3938b44d370182f0
rajeshthuraiyur <rajesh.moorthy@hmcts.net>
2023-03-30 00:14:47 +0100

    Update to increase code coverage for sonar

--- test/unit/service/document-management-service.test.ts
+++ test/unit/service/document-management-service.test.ts
@@ -77,6 +77,15 @@ describe('document-management-service', () => {
       expect(result).to.be.eq('http://someDocumentUrl/');
     });
 
+    it('documentMapToDocStoreUrl should retrieve the doc store url using key', () => {
+      const documentMap: DocumentMap[] = [
+        { id: '00000000-0000-0000-0000-000000000000', url: 'http://someDocumentUrl/' }
+      ];
+      const result = documentIdToDocStoreUrl('00000000-0000-0000-0000-000000000000', documentMap);
+      expect(result).to.be.a('string');
+      expect(result).to.be.eq('http://someDocumentUrl/');
+    });
+
     describe('removeFromDocumentMapper', () => {
       it('should remove a document from DocumentMap when file is found', () => {
         req.session.appeal.documentMap = [

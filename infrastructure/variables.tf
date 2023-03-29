bf2aa4e3aa59445a8c6fce23fb20c95c9f89ee0a
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2023-03-29 17:58:14 +0100

    Revert "To fix terraform variables for doc store release (#905)" (#906)
    This reverts commit 9e9937fc9ce70b2dcdbb64f78d6fedaa658534d1.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,19 +1,24 @@
-variable "product" {  
+variable "product" {
+  type = "string"
 }
 
-variable "component" {  
+variable "component" {
+  type = "string"
 }
 
-variable "location" {  
+variable "location" {
+  type    = "string"
   default = "UK South"
 }
 
-variable "env" {  
+variable "env" {
+  type = "string"
 }
 
-variable "subscription" {  
+variable "subscription" {
+  type = "string"
 }
 
 variable "common_tags" {
-  type = map(string)
+  type = "map"
 }

ec28450d54202a557f9455856e1aa79ab64f7218
Marcello Fabbri <56549133+Marcellofabbri@users.noreply.github.com>
2023-05-12 17:13:07 +0100

    RIA-6516 Copy of PR for supplementary data (#926)

--- app/service/update-appeal-service.ts
+++ app/service/update-appeal-service.ts
@@ -79,7 +79,8 @@ export default class UpdateAppealService {
       },
       data: data,
       event_token: updateEventResponse.token,
-      ignore_warning: true
+      ignore_warning: true,
+      supplementary_data_request: null
     });
     return this.mapCcdCaseToAppeal(ccdCase);
   }

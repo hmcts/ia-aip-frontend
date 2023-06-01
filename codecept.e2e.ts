dd78d544f5efde82c2afef848594590a61fe62cd
Filippo Possenti <filippo.possenti@version1.com>
2023-06-01 14:41:18 +0100

    Attempt without plugins or mocha.

--- codecept.e2e.ts
+++ codecept.e2e.ts
@@ -3,6 +3,12 @@ import config from 'config';
 exports.config = {
   name: 'codecept',
   output: './functional-output/e2e/reports/',
+  bootstrap: async () => {
+    await ourBootStrap();
+  },
+  teardown: async () => {
+    await ourTeardown();
+  },
   helpers: {
     Puppeteer: {
       url: config.get('testUrl'),
@@ -15,29 +21,6 @@ exports.config = {
   gherkin: {
     features: './test/e2e-test/features/*.feature',
     steps: ['./test/e2e-test/step_definitions/steps.ts']
-  },
-  plugins: {
-    retryFailedStep: {
-      enabled: true
-    }
-  },
-  'mocha': {
-    'reporterOptions': {
-      'codeceptjs-cli-reporter': {
-        'stdout': '-',
-        'options': {
-          'verbose': true,
-          'steps': true
-        }
-      },
-      'mochawesome': {
-        'stdout': './functional-output/e2e/reports/console.log',
-        'options': {
-          'reportDir': './functional-output/e2e/reports/',
-          'reportFilename': 'report'
-        }
-      }
-    }
   },
   require: ['ts-node/register/transpile-only']
 };

cbb233a823690267698e53a96690de73f2c5e4fe
filippo-possenti-version1 <109352283+filippo-possenti-version1@users.noreply.github.com>
2023-06-21 22:40:48 +0100

    Reverting multiple commits to re-instate Redis 6 (#963)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,3 +21,26 @@ data "azurerm_key_vault" "ia_key_vault" {
   resource_group_name = "${local.key_vault_name}"
 }
 
+module "redis-cache" {
+  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product     = "${var.product}"
+  location    = var.location
+  env         = var.env
+  private_endpoint_enabled = true
+  redis_version = "6"
+  business_area = "cft" # cft or sds
+  public_network_access_enabled = false
+  common_tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "redis_access_key" {
+  name         = "${var.product}-redis-access-key"
+  value        = "${module.redis-cache.access_key}"
+  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+}
+
+resource "azurerm_key_vault_secret" "redis_connection_string" {
+  name         = "${var.product}-redis-connection-string"
+  value        = "redis://${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+  key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
+}

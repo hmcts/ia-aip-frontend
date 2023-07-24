5d9843a3c01bcce8dae3177f4a1e563e07473759
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-07-24 09:14:50 +0100

    RIA-7638 ref-data-service and ref-data-api

---
+++ test/unit/api/ref-data-api.test.ts
@@ -0,0 +1,30 @@
+import rp from 'request-promise';
+import * as refDataApi from '../../../app/api/ref-data-api';
+import { expect, sinon } from '../../utils/testUtils';
+
+describe('ref-data-api', () => {
+  let rpGetStub: sinon.SinonStub;
+  let rpPostStub: sinon.SinonStub;
+  let sandbox: sinon.SinonSandbox;
+  const headers = {
+    userToken: 'aUserToken',
+    serviceToken: 'aServiceToken'
+  };
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    rpGetStub = sandbox.stub(rp, 'get').resolves({});
+    rpPostStub = sandbox.stub(rp, 'post');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should call common ref data LoV endpoint', async () => {
+    await refDataApi.commonRefDataLov(headers, 'InterpreterLanguage');
+
+    expect(rpPostStub).to.have.been.called;
+  });
+
+});

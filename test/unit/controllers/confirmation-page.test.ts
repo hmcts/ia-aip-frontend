cc4c7a795e9d8175c10ab7765468f0e80ff9afe5
ikirsanov <ivan.kirsanov@version1.com>
2024-03-05 10:58:08 +0100

    RIA-8679: Fix QA comments, remove flag add condition to links for NoRemissionOption

--- test/unit/controllers/confirmation-page.test.ts
+++ test/unit/controllers/confirmation-page.test.ts
@@ -4,10 +4,8 @@ import {
   getConfirmationPaidPage,
   setConfirmationController
 } from '../../../app/controllers/appeal-application/confirmation-page';
-import { FEATURE_FLAGS } from '../../../app/data/constants';
 import { States } from '../../../app/data/states';
 import { paths } from '../../../app/paths';
-import LaunchDarklyService from '../../../app/service/launchDarkly-service';
 import { addDaysToDate } from '../../../app/utils/date-utils';
 import Logger from '../../../app/utils/logger';
 import i18n from '../../../locale/en.json';
@@ -63,16 +61,14 @@ describe('Confirmation Page Controller', () => {
     expect(routerGetStub).to.have.been.calledWith(paths.appealSubmitted.confirmation, middleware);
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, protection, paynow appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, protection, paynow appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.APPEAL_SUBMITTED.id;
     appeal.application.appealType = 'protection';
     appeal.paAppealTypeAipPaymentOption = 'payNow';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: false,
@@ -80,23 +76,18 @@ describe('Confirmation Page Controller', () => {
       paPayNow: true,
       eaHuEu: false,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, protection, payLater appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, protection, payLater appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.APPEAL_SUBMITTED.id;
     appeal.application.appealType = 'protection';
     appeal.paAppealTypeAipPaymentOption = 'payLater';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: false,
@@ -104,23 +95,18 @@ describe('Confirmation Page Controller', () => {
       paPayNow: false,
       eaHuEu: false,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, payment-free', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, payment-free', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.APPEAL_SUBMITTED.id;
     appeal.application.appealType = 'deprivation';
     appeal.paAppealTypeAipPaymentOption = 'payLater';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: false,
@@ -128,22 +114,17 @@ describe('Confirmation Page Controller', () => {
       paPayNow: false,
       eaHuEu: false,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, refusalOfHumanRights appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, refusalOfHumanRights appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.PENDING_PAYMENT.id;
     appeal.application.appealType = 'refusalOfHumanRights';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(14),
       late: false,
@@ -151,22 +132,17 @@ describe('Confirmation Page Controller', () => {
       paPayNow: false,
       eaHuEu: true,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, refusalOfEu appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, refusalOfEu appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.PENDING_PAYMENT.id;
     appeal.application.appealType = 'refusalOfEu';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(14),
       late: false,
@@ -174,22 +150,17 @@ describe('Confirmation Page Controller', () => {
       paPayNow: false,
       eaHuEu: true,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, euSettlementScheme appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for an on time, pending payment, euSettlementScheme appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = false;
     appeal.appealStatus = States.PENDING_PAYMENT.id;
     appeal.application.appealType = 'euSettlementScheme';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(14),
       late: false,
@@ -197,23 +168,18 @@ describe('Confirmation Page Controller', () => {
       paPayNow: false,
       eaHuEu: true,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for a late appeal', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should render confirmation.njk for a late appeal', () => {
     const { appeal } = req.session;
     appeal.application.isAppealLate = true;
     appeal.appealStatus = States.APPEAL_SUBMITTED.id;
     appeal.application.appealType = 'protection';
     appeal.paAppealTypeAipPaymentOption = 'payNow';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: true,
@@ -221,19 +187,14 @@ describe('Confirmation Page Controller', () => {
       paPayNow: true,
       eaHuEu: false,
       appealWithRemissionOption: false,
-      noRemissionOption: false,
-      askHomeOffice: 'A Tribunal Caseworker will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Tribunal Caseworker will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Tribunal Caseworker will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should catch an exception and call next()', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(false);
-
+  it('getConfirmationPage should catch an exception and call next()', () => {
     const error = new Error('the error');
     res.render = sandbox.stub().throws(error);
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(next).to.have.been.calledOnce.calledWith(error);
   });
 
@@ -366,15 +327,13 @@ describe('Confirmation Page Controller', () => {
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an appeal with the remission option', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(true);
-
+  it('getConfirmationPage should render confirmation.njk for an appeal with the remission option', () => {
     const { appeal } = req.session;
     appeal.application.appealType = 'protection';
     appeal.paAppealTypeAipPaymentOption = 'payNow';
     appeal.application.remissionOption = 'asylumSupportFromHo';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: undefined,
@@ -382,23 +341,18 @@ describe('Confirmation Page Controller', () => {
       paPayNow: true,
       eaHuEu: false,
       appealWithRemissionOption: true,
-      noRemissionOption: false,
-      askHomeOffice: 'A Legal Officer will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Legal Officer will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Legal Officer will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: false
     });
   });
 
-  it('getConfirmationPage should render confirmation.njk for an appeal without the remission option', async () => {
-    sandbox.stub(LaunchDarklyService.prototype, 'getVariation').withArgs(req as Request, FEATURE_FLAGS.DLRM_FEE_REMISSION_FEATURE_FLAG, false).resolves(true);
-
+  it('getConfirmationPage should render confirmation.njk for an appeal without the remission option', () => {
     const { appeal } = req.session;
     appeal.application.appealType = 'protection';
     appeal.paAppealTypeAipPaymentOption = 'payNow';
     appeal.application.remissionOption = 'noneOfTheseStatements';
     appeal.application.helpWithFeesOption = 'willPayForAppeal';
 
-    await getConfirmationPage(req as Request, res as Response, next);
+    getConfirmationPage(req as Request, res as Response, next);
     expect(res.render).to.have.been.calledOnce.calledWith('confirmation-page.njk', {
       date: addDaysToDate(5),
       late: undefined,
@@ -406,10 +360,7 @@ describe('Confirmation Page Controller', () => {
       paPayNow: true,
       eaHuEu: false,
       appealWithRemissionOption: false,
-      noRemissionOption: true,
-      askHomeOffice: 'A Legal Officer will ask the Home Office to send any documents it has about your case to the Tribunal',
-      whatToDoNext: 'A Legal Officer will check the Home Office documents and then contact you to tell you what to do next',
-      askHomeOfficeOutOfTime: 'A Legal Officer will look at the reasons your appeal was late and decide if your appeal can continue'
+      noRemissionOption: true
     });
   });
 

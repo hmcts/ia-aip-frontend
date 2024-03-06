d2f051785fbe6051945dce36016b9aa88874a6bc
ikirsanov <ivan.kirsanov@version1.com>
2024-03-06 20:07:20 +0100

    RIA-8679: Fix QA comments, remove flag add condition to links for NoRemissionOption

--- test/unit/utils/remission-utils.test.ts
+++ test/unit/utils/remission-utils.test.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request } from 'express';
 import Logger from '../../../app/utils/logger';
 import { appealHasNoRemissionOption, appealHasRemissionOption } from '../../../app/utils/remission-utils';
 import { expect, sinon } from '../../utils/testUtils';
@@ -6,8 +6,6 @@ import { expect, sinon } from '../../utils/testUtils';
 describe('Remission fields utils', () => {
   let sandbox: sinon.SinonSandbox;
   let req: Partial<Request>;
-  let res: Partial<Response>;
-  let next: NextFunction;
   const logger: Logger = new Logger();
 
   beforeEach(() => {
@@ -30,14 +28,6 @@ describe('Remission fields utils', () => {
         }
       } as any
     } as Partial<Request>;
-
-    res = {
-      render: sandbox.stub(),
-      send: sandbox.stub(),
-      redirect: sinon.spy()
-    } as Partial<Response>;
-
-    next = sandbox.stub() as NextFunction;
   });
 
   afterEach(() => {
@@ -52,34 +42,67 @@ describe('Remission fields utils', () => {
     const remissionOptiondata = [
       {
         remissionOption: 'asylumSupportFromHo',
+        helpWithFeesOption: null,
+        helpWithFeesRefNumber: null,
         expectedResponse: true,
         description: 'asylumSupportFromHo option selected'
       },
       {
         remissionOption: 'feeWaiverFromHo',
+        helpWithFeesOption: null,
+        helpWithFeesRefNumber: null,
         expectedResponse: true,
         description: 'feeWaiverFromHo option selected'
       },
       {
         remissionOption: 'under18GetSupportFromLocalAuthority',
+        helpWithFeesOption: null,
+        helpWithFeesRefNumber: null,
         expectedResponse: true,
         description: 'under18GetSupportFromLocalAuthority option selected'
       },
       {
         remissionOption: 'parentGetSupportFromLocalAuthority',
+        helpWithFeesOption: null,
+        helpWithFeesRefNumber: null,
         expectedResponse: true,
         description: 'parentGetSupportFromLocalAuthority option selected'
       },
       {
         remissionOption: 'noneOfTheseStatements',
+        helpWithFeesOption: 'willPayForAppeal',
+        helpWithFeesRefNumber: null,
+        expectedResponse: false,
+        description: 'noneOfTheseStatements option selected'
+      },
+      {
+        remissionOption: 'noneOfTheseStatements',
+        helpWithFeesOption: 'wantToApply',
+        helpWithFeesRefNumber: 'HWF-123',
+        expectedResponse: true,
+        description: 'noneOfTheseStatements option selected'
+      },
+      {
+        remissionOption: 'noneOfTheseStatements',
+        helpWithFeesOption: 'alreadyApplied',
+        helpWithFeesRefNumber: 'HWF-123',
+        expectedResponse: true,
+        description: 'noneOfTheseStatements option selected'
+      },
+      {
+        remissionOption: 'noneOfTheseStatements',
+        helpWithFeesOption: 'alreadyApplied',
+        helpWithFeesRefNumber: null,
         expectedResponse: false,
         description: 'noneOfTheseStatements option selected'
       }
     ];
 
-    remissionOptiondata.forEach(({ remissionOption, expectedResponse, description }) => {
+    remissionOptiondata.forEach(({ remissionOption, helpWithFeesOption, helpWithFeesRefNumber, expectedResponse, description }) => {
       it(`should be ${description}`, () => {
         appeal.application.remissionOption = remissionOption;
+        appeal.application.helpWithFeesOption = helpWithFeesOption;
+        appeal.application.helpWithFeesRefNumber = helpWithFeesRefNumber;
         expect(appealHasRemissionOption(appeal.application)).to.be.deep.equal(expectedResponse);
       });
     });

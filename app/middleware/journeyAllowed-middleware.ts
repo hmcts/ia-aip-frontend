e50efff7787913115c84768f58e81cb328e17252
ikirsanov <ivan.kirsanov@version1.com>
2024-05-20 13:27:21 +0200

    RIA-8057: Fix functional test

--- app/middleware/journeyAllowed-middleware.ts
+++ app/middleware/journeyAllowed-middleware.ts
@@ -3,7 +3,6 @@ import { paths } from '../paths';
 import { hasPendingTimeExtension } from '../utils/utils';
 
 const isJourneyAllowedMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
   const currentPath: string = req.path;
   const appealStatusPathsCopy = { ...paths[req.session.appeal.appealStatus] } || {};
   const appealStatusPaths = Object.values(appealStatusPathsCopy).map((path: string) => {

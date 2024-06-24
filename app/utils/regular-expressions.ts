c67cd1463b49dafce52247277e13d7253f50b8e8
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2024-06-24 21:27:12 +0100

    DIAC-456 Update postcode search validation unit tests

--- app/utils/regular-expressions.ts
+++ app/utils/regular-expressions.ts
@@ -1,8 +1,11 @@
-const firstRegexSection = '^\\s*([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z]\\d{1,2})|';
-const secondRegexSection = '(([A-Za-z][A-Ha-hJ-Yj-y]\\d{1,2})|(([A-Za-z]\\d[A-Za-z])|';
-const thirdRegexSection = '([A-Za-z][A-Ha-hJ-Yj-y]\\d?[A-Za-z])))) \\d[A-Za-z]{2})\\s*$';
-
-const postcodeRegex = new RegExp(firstRegexSection + secondRegexSection + thirdRegexSection);
+const firstRegexSection = '^\\s*(([Gg][Ii][Rr] 0[Aa]{2})|';
+const secondRegexSection = '((([A-Za-z]\\d{1,2})|';
+const thirdRegexSection = '([A-Za-z][A-Ha-hJ-Yj-y]\\d{1,2})|';
+const fourthRegexSection = '(([A-Za-z]\\d[A-Za-z])|';
+const fifthRegexSection = '([A-Za-z][A-Ha-hJ-Yj-y]\\d?[A-Za-z])))';
+const sixthRegexSection = ' \\d[A-Za-z]{2}))\\s*$';
+const postcodeRegex = new RegExp(firstRegexSection + secondRegexSection + thirdRegexSection
++ fourthRegexSection + fifthRegexSection + sixthRegexSection);
 
 export {
   postcodeRegex

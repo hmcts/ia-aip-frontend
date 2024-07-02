223d942841af7928d6004af59500ab9b7a3b8394
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2024-07-02 13:15:30 +0100

    DIAC-161 Update middleware file

--- app/middleware/document-download-middleware.ts
+++ app/middleware/document-download-middleware.ts
@@ -11,47 +11,35 @@ export class DocumentDownloadMiddleware {
   public enableFor(app: Application): void {
     log.info('Before app.use in DocumentDownloadMiddleware');
     app.use(
-                proxyList.endpoint,
-                (req, res, next) => {
-                  log.info('Entering DocumentDownloadMiddleware with proxy endpoint '
-                      + proxyList.endpoint(req) + ' and proxy path ' + proxyList.path(req));
-                  log.info(`DocumentDownloadMiddleware Request URL: ${req.url},
-                    Headers: ${JSON.stringify(req.headers)}`);
-                  next();
-                },
-                proxy(config.get('cdamDocumentManagement.apiUrl'), {
-                  proxyReqPathResolver: proxyList.path,
-                  // proxyReqOptDecorator: this.addCdamHeaders,
-                  secure: false,
-                  changeOrigin: true,
-                  proxyErrorHandler: (err, res, next) => {
-                    if (err instanceof UserNotLoggedInError) {
-                      return res.redirect('/login');
-                    } else if (err.status === 401) {
-                      return res.redirect('/login'); // TODO: define path to redirect to
-                    } else if (err.code === 'ECONNRESET') {
-                      log.info('Connection reset by peer. URL: ' + res.req.path);
-                      return res.redirect('/login'); // TODO: define path to redirect to
-                    }
-                    next(err);
-                  }
-                })
-            );
+         (req, res, next) => {
+           log.info(
+                    'Entering DocumentDownloadMiddleware with proxy endpoint ' +
+                    proxyList.endpoint(req) + ' and proxy path ' + proxyList.path(req)
+                );
+           log.info(
+                    `DocumentDownloadMiddleware Request URL: ${req.url}, Headers: ${JSON.stringify(req.headers)}`
+                );
+           next();
+         },
+            proxy(config.get('cdamDocumentManagement.apiUrl'), {
+              proxyReqPathResolver: (req) => proxyList.path(req),
+                // proxyReqOptDecorator: this.addCdamHeaders,
+              secure: false,
+              changeOrigin: true,
+              proxyErrorHandler: (err, res, next) => {
+                if (err instanceof UserNotLoggedInError) {
+                  return res.redirect('/login');
+                } else if (err.status === 401) {
+                  return res.redirect('/login'); // TODO: define path to redirect to
+                } else if (err.code === 'ECONNRESET') {
+                  log.info('Connection reset by peer. URL: ' + res.req.path);
+                  return res.redirect('/login'); // TODO: define path to redirect to
+                }
+                next(err);
+              }
+            })
+        );
   }
-
-// addCdamHeaders(
-//     proxyReqOpts: { headers: Record<string, unknown> },
-//     req: Request
-// ): { headers: Record<string, unknown> } {
-//     if (!req.session.user) {
-//         throw new UserNotLoggedInError();
-//     }
-//
-//     proxyReqOpts.headers['ServiceAuthorization'] = this.authenticationService.getSecurityHeaders(req);
-//     proxyReqOpts.headers['Authorization'] = `Bearer ${req.session.user.accessToken}`;
-//     proxyReqOpts.headers['user-roles'] = 'citizen';
-//     return proxyReqOpts;
-// }
 }
 
 class UserNotLoggedInError extends Error {
@@ -59,3 +47,11 @@ class UserNotLoggedInError extends Error {
     super('Error downloading document. User not logged in.');
   }
 }
+
+// Uncomment and implement addCdamHeaders if needed
+// addCdamHeaders(
+//     proxyReqOpts: { headers: Record<string, unknown> },
+//     req: Request
+// ): { headers: Record<string, unknown> } {
+//     if (!req.session.user) {
+//

a40cede17849aafcab53d4c8041faf665f74b6bd
jamiephillipshmcts <jamie.phillips@justice.gov.uk>
2024-07-05 16:51:30 +0100

    DIAC-523 Allow space in middle of postcode (regex)

--- app/utils/regular-expressions.ts
+++ app/utils/regular-expressions.ts
@@ -3,7 +3,7 @@ const secondRegexSection = '((([A-Za-z]\\d{1,2})|';
 const thirdRegexSection = '([A-Za-z][A-Ha-hJ-Yj-y]\\d{1,2})|';
 const fourthRegexSection = '(([A-Za-z]\\d[A-Za-z])|';
 const fifthRegexSection = '([A-Za-z][A-Ha-hJ-Yj-y]\\d?[A-Za-z])))';
-const sixthRegexSection = ' \\d[A-Za-z]{2}))\\s*$';
+const sixthRegexSection = '\\s?\\d[A-Za-z]{2}))\\s*$';
 const postcodeRegex = new RegExp(firstRegexSection + secondRegexSection + thirdRegexSection
 + fourthRegexSection + fifthRegexSection + sixthRegexSection);
 

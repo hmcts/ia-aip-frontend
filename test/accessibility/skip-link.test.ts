1e7cd8eab217e854cdebce1b5376e5282dc8ee51
Phoebe Revolta <phoebe.revolta@solirius.com>
2024-09-11 13:48:13 +0100

    DIAC-566: add tests

---
+++ test/accessibility/skip-link.test.ts
@@ -0,0 +1,66 @@
+import nunjucks from 'nunjucks';
+import i18n from '../../locale/en.json';
+const { expect } = require('chai');
+import { JSDOM } from 'jsdom';
+
+describe('"Skip to Main Content" link rendering', () => {
+
+  const env = nunjucks.configure([
+    'node_modules/govuk-frontend',
+    'views'
+  ]);
+
+  env.addFilter('eval', function(text: string) {
+    return nunjucks.renderString(text, this.ctx);
+  });
+
+  env.addGlobal('i18n', i18n);
+
+  const skipToMainContentLink = '<a href="#main-content" class="govuk-skip-link" data-module="govuk-skip-link">Skip to main content</a>';
+
+  it('should render the "Skip to Main Content" link in layout.njk with href="#main-content"', () => {
+
+    const rendered = env.render('layout.njk');
+
+    expect(rendered).to.include(skipToMainContentLink);
+  });
+
+  it('should render the "Skip to Main Content" link in guidanceLayout.njk with href="#main-content"', () => {
+
+    const rendered = env.render('guidanceLayout.njk');
+
+    expect(rendered).to.include(skipToMainContentLink);
+  });
+
+});
+
+describe('"#main-content" rendering', () => {
+
+  const env = nunjucks.configure([
+    'node_modules/govuk-frontend',
+    'views'
+  ]);
+
+  env.addFilter('eval', function(text: string) {
+    return nunjucks.renderString(text, this.ctx);
+  });
+
+  env.addGlobal('i18n', i18n);
+
+  const mainContent = '<main class="govuk-main-wrapper " id="main-content" role="main" tabindex="-1">';
+
+  it('should render "#main-content" in layout.njk with tabindex="-1"', () => {
+
+    const rendered = env.render('layout.njk');
+
+    expect(rendered).to.include(mainContent);
+  });
+
+  it('should render "#main-content" in guidanceLayout.njk with tabindex="-1"', () => {
+
+    const rendered = env.render('guidanceLayout.njk');
+
+    expect(rendered).to.include(mainContent);
+  });
+
+});

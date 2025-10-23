ee13114128ac3e1f3a419b9e2aa31c28e3da6e3d
nilay <nilay.patel@solirius.com>
2025-10-23 13:22:50 +0100

    fixing test title list appending

--- test/e2e-test/helpers/failedTestHelper.ts
+++ test/e2e-test/helpers/failedTestHelper.ts
@@ -5,15 +5,13 @@ import * as testStateHelper from '../testStateHelper';
 class FailedTest extends helper {
   _failed(test: any) {
     console.log('Failed test: ' + test.title);
+    testStateHelper.addTestTitle(test.title);
     testStateHelper.setTestFailed(true);
   }
 
   _passed(test: any) {
-    testStateHelper.incrementTestsPassed();
-  }
-
-  _after(test: any) {
     testStateHelper.addTestTitle(test.title);
+    testStateHelper.incrementTestsPassed();
   }
 }
 

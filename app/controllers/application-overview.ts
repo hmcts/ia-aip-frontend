e7f049e064b8917c0b457524799e027e1677c990
tiagreaney <tiagreaney@gmail.com>
2025-10-30 13:54:10 +0000

    DIAC-1428 DTSHoSEAssurance - Upgrade Gov.uk Frontend package

--- app/controllers/application-overview.ts
+++ app/controllers/application-overview.ts
@@ -163,6 +163,7 @@ function getApplicationOverview(updateAppealService: UpdateAppealService) {
       const application = req.session.appeal.application;
 
       const showAskForFeeRemission = refundFeatureEnabled
+        && 'Paid' === paymentStatus
         && (!application.refundRequested || application.refundRequested && !!application.remissionDecision);
 
       const showAskForSomethingInEndedState = refundFeatureEnabled && showAppealRequestsInAppealEndedStatus;

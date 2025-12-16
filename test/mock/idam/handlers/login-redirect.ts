e80c658253783201000cc3b7bf62e02a56722bed
nilay <nilay.patel@solirius.com>
2025-12-16 16:12:09 +0000

    fix

--- test/mock/idam/handlers/login-redirect.ts
+++ test/mock/idam/handlers/login-redirect.ts
@@ -8,23 +8,19 @@ const logLabel: string = getLogLabel(__filename);
 export async function setupLoginRedirect(server: Mockttp) {
   await server.forPost('/login').thenCallback(async (request) => {
     const bodyFormData = await request.body.getFormData();
+    const username = bodyFormData['body'] as string;
+    const state = bodyFormData['state'] as string;
+    const redirectUri = bodyFormData['redirect_uri'] as string;
     for (let bodyFormDataKey in bodyFormData) {
       if (typeof bodyFormData[bodyFormDataKey] === 'string') {
         logger.trace('key: ' + bodyFormDataKey, logLabel);
         logger.trace('value: ' + bodyFormData[bodyFormDataKey] as string, logLabel);
-      } else {
-        for (let filePart of bodyFormData[bodyFormDataKey] as string) {
-          logger.trace('filePart: ' + filePart, logLabel);
-        }
       }
     }
-
-    const body = await request.body.getJson() as any;
-    const { username, state, redirect_uri } = body;
     cache.put('email', username);
 
     const stateParam = state ? `&state=${state}` : '';
-    const redirectUrl = `${redirect_uri}?code=123${stateParam}`;
+    const redirectUrl = `${redirectUri}?code=123${stateParam}`;
 
     return {
       status: 302,

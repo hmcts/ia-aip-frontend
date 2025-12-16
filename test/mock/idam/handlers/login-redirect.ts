91aa9edae0ed93ec55bbd08208c4394fadda6c2e
nilay <nilay.patel@solirius.com>
2025-12-16 15:38:40 +0000

    debug logging

--- test/mock/idam/handlers/login-redirect.ts
+++ test/mock/idam/handlers/login-redirect.ts
@@ -7,17 +7,14 @@ const logLabel: string = getLogLabel(__filename);
 
 export async function setupLoginRedirect(server: Mockttp) {
   await server.forPost('/login').thenCallback(async (request) => {
-    const bodyJson = await request.body.getJson();
-    const bodyText = await request.body.getText();
     const bodyFormData = await request.body.getFormData();
-    logger.trace(bodyText, logLabel);
-    logger.trace(JSON.stringify(bodyJson, null, 2), logLabel);
     for (let bodyFormDataKey in bodyFormData) {
       if (typeof bodyFormData[bodyFormDataKey] === 'string') {
-        logger.trace(bodyFormData[bodyFormDataKey] as string, logLabel);
+        logger.trace('key: ' + bodyFormDataKey, logLabel);
+        logger.trace('value: ' + bodyFormData[bodyFormDataKey] as string, logLabel);
       } else {
         for (let filePart of bodyFormData[bodyFormDataKey] as string) {
-          logger.trace(filePart, logLabel);
+          logger.trace('filePart: ' + filePart, logLabel);
         }
       }
     }

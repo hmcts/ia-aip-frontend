415fcaab57deab1544835e1b6cca46586f3c1561
nilay <nilay.patel@solirius.com>
2025-12-16 16:12:38 +0000

    fix

--- test/mock/idam/handlers/login-redirect.ts
+++ test/mock/idam/handlers/login-redirect.ts
@@ -1,9 +1,5 @@
 import cache from 'memory-cache';
 import { Mockttp } from 'mockttp';
-import Logger, { getLogLabel } from '../../../../app/utils/logger';
-
-const logger: Logger = new Logger();
-const logLabel: string = getLogLabel(__filename);
 
 export async function setupLoginRedirect(server: Mockttp) {
   await server.forPost('/login').thenCallback(async (request) => {
@@ -11,12 +7,6 @@ export async function setupLoginRedirect(server: Mockttp) {
     const username = bodyFormData['body'] as string;
     const state = bodyFormData['state'] as string;
     const redirectUri = bodyFormData['redirect_uri'] as string;
-    for (let bodyFormDataKey in bodyFormData) {
-      if (typeof bodyFormData[bodyFormDataKey] === 'string') {
-        logger.trace('key: ' + bodyFormDataKey, logLabel);
-        logger.trace('value: ' + bodyFormData[bodyFormDataKey] as string, logLabel);
-      }
-    }
     cache.put('email', username);
 
     const stateParam = state ? `&state=${state}` : '';

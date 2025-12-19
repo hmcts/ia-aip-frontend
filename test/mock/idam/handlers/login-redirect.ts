2b78d752bb885cb9321cb617f5c03bd2eb458cc3
nilay <nilay.patel@solirius.com>
2025-12-19 15:53:11 +0000

    logging

--- test/mock/idam/handlers/login-redirect.ts
+++ test/mock/idam/handlers/login-redirect.ts
@@ -1,31 +1,26 @@
 import cache from 'memory-cache';
-import { CompletedBody, CompletedRequest, Mockttp } from 'mockttp';
+import { CompletedRequest, Mockttp } from 'mockttp';
+import querystring from 'querystring';
 
 export async function setupLoginRedirect(server: Mockttp) {
   await server.forPost('/login').thenCallback(async (request: CompletedRequest) => {
-    const rawBody: CompletedBody = request.body;
-    // tslint:disable:no-console
-    console.log('rawBody: ' + rawBody);
-    const json = await rawBody.getJson();
-    console.log('json: ' + json);
-    const buffer = await rawBody.getDecodedBuffer();
-    console.log('buffer: ' + buffer);
-    const text = await rawBody.getText();
-    console.log('text: ' + text);
-    const formData = await rawBody.getFormData();
-    console.log('formData: ' + formData);
-    for (let formDataKey in formData) {
-      console.log('formDataKey: ' + formDataKey);
-      console.log('formData value: ' + formData[formDataKey]);
-    }
-    const bufferString = buffer.toString();
-    console.log('bufferString: ' + bufferString);
-    const body = JSON.parse(json.toString());
-    console.log('body: ' + JSON.stringify(body));
-    cache.put('email', body.username);
+    const rawBody = request.body;
 
-    const stateParam = body.state ? `&state=${body.state}` : '';
-    const redirectUrl = `${body.redirect_uri}?code=123${stateParam}`;
+    const text = await rawBody.getText();
+
+    const body = querystring.parse(text);
+
+    const username = body.username as string;
+    const redirectUri = body.redirect_uri as string;
+    const state = body.state as string | undefined;
+    // tslint:disable:no-console
+    console.log(username);
+    console.log(redirectUri);
+    console.log(state);
+    cache.put('email', username);
+
+    const stateParam = state ? `&state=${state}` : '';
+    const redirectUrl = `${redirectUri}?code=123${stateParam}`;
 
     return {
       statusCode: 302,

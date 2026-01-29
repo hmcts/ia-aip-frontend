0b8b8d3bb99039f1c9dcc7d2676fc087f7182c43
nilay <nilay.patel@solirius.com>
2026-01-29 17:46:48 +0000

    test failure check fix - fails if no tests run (or seen to run)

--- test/functional/bootstrap.ts
+++ test/functional/bootstrap.ts
@@ -81,7 +81,7 @@ export function failureCheck() {
   console.log('Total scenarios run: ' + uniqueTitles.length);
   console.log('Scenarios passed: ' + testState.testsPassed.length);
   console.log('---------------------');
-  if (testState.testsPassed.length === uniqueTitles.length) {
+  if (uniqueTitles.length > 0 && testState.testsPassed.length === uniqueTitles.length) {
     process.exit(0);
   } else {
     const failedTests = uniqueTitles.filter(title => !testState.testsPassed.includes(title));

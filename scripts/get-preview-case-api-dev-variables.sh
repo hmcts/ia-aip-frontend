eed54bfa7ed2931f1cd5451e98ba8cf41c17c95c
nilay <nilay.patel@solirius.com>
2026-02-20 15:48:06 +0000

    allowing dev on local pointing to ia-case-api preview instance

--- scripts/get-preview-case-api-dev-variables.sh
+++ scripts/get-preview-case-api-dev-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+if [ -z "$1" ]; then
+    echo "Missing ia-case-api PR number. Usage: $0 <PR_NUMBER>"
+    exit 1
+fi
+
+PR_NUMBER=$1
 VAULT=ia-aat
 MAPPING=s2s-secret:S2S_SECRET,idam-secret:IDAM_SECRET,system-password:IA_SYSTEM_PASSWORD,launch-darkly-sdk-key:LAUNCH_DARKLY_SDK_KEY,addressLookupToken:ADDRESS_LOOKUP_TOKEN,saucelabs:SAUCELABS_SECRET,pcq-token-key:PCQ_TOKEN_KEY,docmosis-access-key:DOCMOSIS_ACCESS_KEY,ia-gov-notify-key:GOV_NOTIFY_KEY,system-username:IA_SYSTEM_USERNAME,test-caseofficer-username:TEST_CASEOFFICER_USERNAME,test-homeoffice-generic-username:TEST_HOMEOFFICE_GENERIC_USERNAME,test-adminofficer-username:TEST_ADMINOFFICER_USERNAME,test-judge-x-username:TEST_JUDGE_X_USERNAME,test-law-firm-share-case-a-username:TEST_LAW_FIRM_SHARE_CASE_A_USERNAME,hearing-centre-bradford-email:IA_HEARING_CENTRE_BRADFORD_EMAIL,hearing-centre-manchester-email:IA_HEARING_CENTRE_MANCHESTER_EMAIL,hearing-centre-newport-email:IA_HEARING_CENTRE_NEWPORT_EMAIL,hearing-centre-taylor-house-email:IA_HEARING_CENTRE_TAYLOR_HOUSE_EMAIL,hearing-centre-north-shields-email:IA_HEARING_CENTRE_NORTH_SHIELDS_EMAIL,hearing-centre-birmingham-email:IA_HEARING_CENTRE_BIRMINGHAM_EMAIL,hearing-centre-hatton-cross-email:IA_HEARING_CENTRE_HATTON_CROSS_EMAIL,hearing-centre-glasgow-email:IA_HEARING_CENTRE_GLASGOW_EMAIL,customer-services-email:IA_CUSTOMER_SERVICES_EMAIL
 
@@ -7,7 +13,7 @@ MAPPING=s2s-secret:S2S_SECRET,idam-secret:IDAM_SECRET,system-password:IA_SYSTEM_
 mapping_arr=("${(@s/,/)MAPPING}")
 
 # Output file
-OUTPUT_FILE="aat.env"
+OUTPUT_FILE="preview.env"
 : > "$OUTPUT_FILE" # Clear the file if it exists
 
 for i in {1..${#mapping_arr[@]}}; do
@@ -23,6 +29,7 @@ for i in {1..${#mapping_arr[@]}}; do
 done
 
 echo "IA_CUSTOMER_SERVICES_TELEPHONE=111123111" >> "$OUTPUT_FILE"
-echo "CCD_API_URL=http://ccd-data-store-api-aat.service.core-compute-aat.internal" >> "$OUTPUT_FILE"
-echo "IA_CASE_PAYMENTS_API_URL=http://ia-case-payments-api-aat.service.core-compute-aat.internal" >> "$OUTPUT_FILE"
-echo "PCQ_RETURN_URL=immigration-appeal.aat.platform.hmcts.net/about-appeal" >> "$OUTPUT_FILE"
+echo "CCD_API_URL=https://ccd-data-store-api-ia-case-api-pr-${PR_NUMBER}.preview.platform.hmcts.net" >> "$OUTPUT_FILE"
+echo "IA_CASE_PAYMENTS_API_URL=https://ia-case-api-pr-${PR_NUMBER}-payments-api.preview.platform.hmcts.net" >> "$OUTPUT_FILE"
+echo "PCQ_RETURN_URL=https://localhost:3000/about-appeal" >> "$OUTPUT_FILE"
+

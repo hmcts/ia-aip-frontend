dfbb4b035302585958a2445cefa658aaae7af54b
nilay <nilay.patel@solirius.com>
2026-03-10 13:54:28 +0000

    making other modals extend one common one

--- client/delete-modal.ts
+++ client/delete-modal.ts
@@ -1,75 +1,48 @@
 import { paths } from '../app/paths';
 import i18n from '../locale/en.json';
+import ConfirmModal from './confirm-modal';
 
-export default class DeleteModal {
-  private confirmButtonElement: HTMLElement = null;
-  private cancelButtonElement: HTMLElement = null;
-  private linkToModalElement: NodeListOf<HTMLElement> = null;
-  private modalElement: HTMLElement = null;
-  private modalOverlayElement: HTMLElement = null;
+export default class DeleteModal extends ConfirmModal{
   private descriptionElement: HTMLElement = null;
-  private focusableElements: NodeListOf<Element> = null;
-  private firstFocusableElement: HTMLElement = null;
-  private lastFocusableElement: HTMLElement = null;
-  private previousFocusedElement: HTMLElement = null;
-  private scrollPosition: number = null;
-  private body: HTMLElement = null;
   private currentCaseId: string = null;
-
+  private linksToModalElement: NodeListOf<Element> = null;
   constructor() {
+    super();
     const modalId = 'delete-draft-modal';
-    this.init = this.init.bind(this);
     this.modalElement = document.querySelector(`#${modalId}`);
     this.modalOverlayElement = document.querySelector(`#${modalId}-overlay`);
     this.confirmButtonElement = document.querySelector(`#${modalId}-confirm`);
     this.cancelButtonElement = document.querySelector(`#${modalId}-cancel`);
     this.descriptionElement = document.querySelector(`#${modalId}-description`);
-    this.linkToModalElement = document.querySelectorAll(`.${i18n.pages.casesList.deleteLinkClass}`);
-    this.body = document.querySelector('body');
-    this.focusableElements =
-      this.modalElement?.querySelectorAll(
-        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-      ) || ([] as unknown as NodeListOf<Element>);
-    this.firstFocusableElement = this.focusableElements[0] as HTMLElement;
-    this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1] as HTMLElement;
+    this.linksToModalElement = document.querySelectorAll(`.${i18n.pages.casesList.deleteLinkClass}`);
+    this.setupModal();
   }
-
-  init() {
-    this.addLinkListeners();
-  }
-
-  addButtonListeners = () => {
-    if (this.confirmButtonElement) this.confirmButtonElement.addEventListener('click', this.doAction);
-    if (this.cancelButtonElement) this.cancelButtonElement.addEventListener('click', this.closeModal);
-  };
-
-  removeButtonListeners = () => {
-    if (this.confirmButtonElement) this.confirmButtonElement.removeEventListener('click', this.doAction);
-    if (this.cancelButtonElement) this.cancelButtonElement.removeEventListener('click', this.closeModal);
-  };
 
   doAction = () => {
     window.location.href = paths.common.deleteDraftAppeal.replace(':id', this.currentCaseId);
   };
 
   addLinkListeners = () => {
-    if (this.linkToModalElement) {
-      this.linkToModalElement
+    if (this.linksToModalElement) {
+      this.linksToModalElement
         .forEach(element => {
-          element.addEventListener('click', this.openModal);
-          element.addEventListener('keydown', this.openModal);
+          element.addEventListener('click', this.openModalWithContent);
+          element.addEventListener('keydown', this.openModalWithContent);
         });
     }
   };
 
   removeLinkListeners = () => {
-    if (this.linkToModalElement) {
-      this.linkToModalElement
-        .forEach(element => element.removeEventListener('click', this.openModal));
+    if (this.linksToModalElement) {
+      this.linksToModalElement
+        .forEach(element => {
+          element.removeEventListener('click', this.openModalWithContent);
+          element.removeEventListener('keydown', this.openModalWithContent);
+        });
     }
   };
 
-  openModal = (event: Event) => {
+  openModalWithContent = (event: Event) => {
     if (event.type === 'keydown' && (event as KeyboardEvent).key !== 'Enter') return;
     if (event.type === 'keydown' && (event as KeyboardEvent).key === 'Enter') {
       event.preventDefault();
@@ -77,63 +50,11 @@ export default class DeleteModal {
     const target = event.currentTarget as HTMLElement;
     const caseId = target.dataset.caseId;
     this.currentCaseId = caseId;
-    this.updateModalContent(caseId);
-
-    this.modalElement.removeAttribute('aria-hidden');
-    this.modalOverlayElement.removeAttribute('aria-hidden');
-    this.previousFocusedElement = document.activeElement as HTMLElement;
-    this.lastFocusableElement.focus();
-    this.disableScroll();
-    this.body.addEventListener('keydown', this.keyDownEventListener);
-    this.addButtonListeners();
-    this.removeLinkListeners();
-  };
-
-  updateModalContent = (caseId: string) => {
     if (this.descriptionElement) {
       this.descriptionElement.textContent = i18n.pages.casesList.deleteDraftModal.description
         .replace('{{ caseId }}', caseId);
     }
-  };
 
-  disableScroll = () => {
-    this.scrollPosition = window.pageYOffset;
-    this.body.style.overflow = 'hidden';
-    this.body.style.position = 'fixed';
-    this.body.style.width = '100%';
-    this.body.style.top = `-${this.scrollPosition}px`;
-  };
-
-  enableScroll = () => {
-    this.body.style.removeProperty('overflow');
-    this.body.style.removeProperty('position');
-    this.body.style.removeProperty('width');
-    window.scrollTo(0, this.scrollPosition);
-  };
-
-  closeModal = () => {
-    this.modalElement.setAttribute('aria-hidden', 'true');
-    this.modalOverlayElement.setAttribute('aria-hidden', 'true');
-
-    document.querySelector('body').removeEventListener('keydown', this.keyDownEventListener);
-    if (this.previousFocusedElement) this.previousFocusedElement.focus();
-    this.enableScroll();
-    this.removeButtonListeners();
-    this.addLinkListeners();
-  };
-
-  keyDownEventListener = (event) => {
-    if (event.key !== 'Tab') return;
-    if (event.shiftKey) {
-      if (document.activeElement === this.firstFocusableElement) {
-        event.preventDefault();
-        this.lastFocusableElement.focus();
-      }
-    } else {
-      if (document.activeElement === this.lastFocusableElement) {
-        event.preventDefault();
-        this.firstFocusableElement.focus();
-      }
-    }
+    this.openModal();
   };
 }
--- client/session-timeout.ts
+++ client/session-timeout.ts
@@ -2,49 +2,42 @@ import axios from 'axios';
 import moment from 'moment';
 import { paths } from '../app/paths';
 import i18n from '../locale/en.json';
+import ConfirmModal from './confirm-modal';
 
-export default class SessionTimeout {
+export default class SessionTimeout extends ConfirmModal {
   public sessionExpirationTime: string;
   public sessionTimeoutCountdown: number;
   public bufferSessionExtension: number = 2 * 60 * 1000;
   private sessionTimeout: number;
   private modalTimeout: number;
   private modalCountdown: number;
-  private extendSessionElement: HTMLElement = null;
-  private modalElement: HTMLElement = null;
-  private modalOverlayElement: HTMLElement = null;
   private modalCountdownElement: HTMLElement = null;
-  private focusableElements: NodeListOf<Element> = null;
-  private firstFocusableElement: HTMLElement = null;
-  private lastFocusableElement: HTMLElement = null;
-  private previousFocusedElement: HTMLElement = null;
-  private scrollPosition: number = null;
-  private body: HTMLElement = null;
 
   constructor() {
+    super();
     this.init = this.init.bind(this);
     this.modalElement = document.querySelector('#timeout-modal');
     this.modalOverlayElement = document.querySelector('#modal-overlay');
     this.modalCountdownElement = document.querySelector('#dialog-description');
-    this.extendSessionElement = document.querySelector('#extend-session');
+    this.confirmButtonElement = document.querySelector('#extend-session');
     this.body = document.querySelector('body');
-    this.focusableElements = this.modalElement.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
-
-    this.firstFocusableElement = this.focusableElements[0] as HTMLElement;
-    this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1] as HTMLElement;
+    this.setupModal();
   }
 
   init() {
-    this.addListeners();
-    void this.extendSession();
+    this.addButtonListeners();
+    void this.doAction();
   }
 
-  addListeners = () => {
-    if (this.extendSessionElement) this.extendSessionElement.addEventListener('click', this.extendSession);
-  };
-
-  removeListeners = () => {
-    if (this.extendSessionElement) this.extendSessionElement.removeEventListener('click', this.extendSession);
+  doAction = (): Promise<void> => {
+    return axios.get(paths.common.extendSession).then((response: any): void => {
+      this.sessionExpirationTime = response.data.timeout;
+      this.restartCounters();
+      this.closeModal();
+    }).catch((e) => {
+      this.removeButtonListeners();
+      this.stopCounters();
+    });
   };
 
   startCounter = () => {
@@ -87,70 +80,7 @@ export default class SessionTimeout {
     }, 1000);
   };
 
-  openModal = () => {
-    this.modalElement.removeAttribute('aria-hidden');
-    this.modalOverlayElement.removeAttribute('aria-hidden');
-    this.previousFocusedElement = document.activeElement as HTMLElement;
-    this.firstFocusableElement.focus();
-    this.disableScroll();
-    this.body.addEventListener('keydown', this.keyDownEventListener);
-  };
-
-  disableScroll = () => {
-    this.scrollPosition = window.pageYOffset;
-    this.body.style.overflow = 'hidden';
-    this.body.style.position = 'fixed';
-    this.body.style.width = '100%';
-    this.body.style.top = `-${this.scrollPosition}px`;
-  };
-
-  enableScroll = () => {
-    this.body.style.removeProperty('overflow');
-    this.body.style.removeProperty('position');
-    this.body.style.removeProperty('width');
-    window.scrollTo(0, this.scrollPosition);
-  };
-
-  closeModal = () => {
-    this.modalElement.setAttribute('aria-hidden', 'true');
-    this.modalOverlayElement.setAttribute('aria-hidden', 'true');
-
-    document.querySelector('body').removeEventListener('keydown', this.keyDownEventListener);
-    if (this.previousFocusedElement) this.previousFocusedElement.focus();
-    this.enableScroll();
-  };
-
   signOut() {
     window.location.assign(paths.common.sessionExpired);
   }
-
-  extendSession = (): Promise<void> => {
-    return axios.get(paths.common.extendSession).then((response: any): void => {
-      this.sessionExpirationTime = response.data.timeout;
-      this.restartCounters();
-      this.closeModal();
-    }).catch((e) => {
-      this.removeListeners();
-      this.stopCounters();
-    });
-  };
-
-  keyDownEventListener = (event) => {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      if (!this.modalElement.contains(document.activeElement) || this.focusableElements.length === 1) {
-        this.firstFocusableElement.focus();
-        return;
-      }
-      if (event.shiftKey) {
-        if (document.activeElement === this.firstFocusableElement) {
-          this.lastFocusableElement.focus();
-        }
-      } else {
-        if (document.activeElement === this.lastFocusableElement) {
-          this.firstFocusableElement.focus();
-        }
-      }
-    }
-  };
 }

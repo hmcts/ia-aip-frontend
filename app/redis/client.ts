08e2c7e4f03babdb42cde6da727bba011eceff3d
nilay <nilay.patel@solirius.com>
2026-03-11 12:12:05 +0000

    fixing redis not running in unit tests issue

--- app/redis/client.ts
+++ app/redis/client.ts
@@ -1,7 +1,8 @@
 import config from 'config';
 const redis = require('redis');
+const useRedis: boolean = config.get('session.useRedis') === true;
 
-export const redisClient = redis.createClient({
+export const redisClient = useRedis ? redis.createClient({
   url: config.get('session.redis.url'),
   ttl: config.get('session.redis.ttlInSeconds')
-});
+}) : { send_command: async () => undefined };

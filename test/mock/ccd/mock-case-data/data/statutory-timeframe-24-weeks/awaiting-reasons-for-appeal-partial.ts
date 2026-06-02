99c121e5d687773056fae6467df92c8048252ead
Nilay Patel <nilay.patel@solirius.com>
2026-06-02 16:55:19 +0100

    cleaning up mocks for functional tests (#1492)
    * cleaning up mocks for functional tests
    
    * cleaning up mocks for functional tests

---
+++ test/mock/ccd/mock-case-data/data/statutory-timeframe-24-weeks/awaiting-reasons-for-appeal-partial.ts
@@ -0,0 +1,10 @@
+import awaitingReasonsForAppealCaseDataSTF24W from './awaiting-reasons-for-appeal';
+
+export default {
+  ...awaitingReasonsForAppealCaseDataSTF24W,
+  'id': 46,
+  'case_data': {
+    ...awaitingReasonsForAppealCaseDataSTF24W.case_data,
+    'reasonsForAppealDecision': 'test'
+  }
+};

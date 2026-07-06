bc73068ef5de82f705ba83ef51b9a306276cf25a
Velican <duzgunce.vd@gmail.com>
2026-07-06 15:33:25 +0100

    update pages with config

--- app/controllers/appeal-application/home-office-details.ts
+++ app/controllers/appeal-application/home-office-details.ts
@@ -1,3 +1,4 @@
+import config from 'config';
 import { NextFunction, Request, Response, Router } from 'express';
 import _ from 'lodash';
 import moment from 'moment';
@@ -9,7 +10,7 @@ import UpdateAppealService from '../../service/update-appeal-service';
 import { getNationalitiesOptions } from '../../utils/nationalities';
 import { shouldValidateWhenSaveForLater } from '../../utils/save-for-later-utils';
 import { getConditionalRedirectUrl } from '../../utils/url-utils';
-import { getRedirectPage, toIsoDate } from '../../utils/utils';
+import { asBooleanValue, getRedirectPage, toIsoDate } from '../../utils/utils';
 import {
   appellantNamesValidation,
   createStructuredError,
@@ -20,6 +21,8 @@ import {
   nationalityValidation
 } from '../../utils/validations/fields-validations';
 
+const homeOfficeValidationEnabled = asBooleanValue(config.get('features.homeOfficeValidationEnabled'));
+
 function getHomeOfficeDetails(req: Request, res: Response, next: NextFunction) {
   try {
     req.session.appeal.application.isEdit = _.has(req.query, 'edit');
@@ -27,7 +30,8 @@ function getHomeOfficeDetails(req: Request, res: Response, next: NextFunction) {
     const { homeOfficeRefNumber } = req.session.appeal.application || null;
     res.render('appeal-application/home-office/details.njk', {
       homeOfficeRefNumber,
-      previousPage: paths.appealStarted.taskList
+      previousPage: paths.appealStarted.taskList,
+      homeOfficeValidationEnabled
     });
   } catch (e) {
     next(e);
@@ -99,7 +103,8 @@ function getNamePage(req: Request, res: Response, next: NextFunction) {
     const previousPage = outsideUkWhenApplicationMade ? paths.appealStarted.gwfReference : paths.appealStarted.details;
     return res.render('appeal-application/personal-details/name.njk', {
       personalDetails,
-      previousPage
+      previousPage,
+      homeOfficeValidationEnabled
     });
   } catch (e) {
     next(e);
@@ -183,7 +188,8 @@ function getDateOfBirthPage(req: Request, res: Response, next: NextFunction) {
     const dob = application.personalDetails && application.personalDetails.dob || null;
     return res.render('appeal-application/personal-details/date-of-birth.njk', {
       dob,
-      previousPage: paths.appealStarted.name
+      previousPage: paths.appealStarted.name,
+      homeOfficeValidationEnabled
     });
   } catch (e) {
     next(e);

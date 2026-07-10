f90f78b94fb8464d6b1f003561b4fd34ea67f94d
Velican <duzgunce.vd@gmail.com>
2026-07-10 12:30:28 +0100

    add unit tests

--- test/unit/controllers/enter-name-page.test.ts
+++ test/unit/controllers/enter-name-page.test.ts
@@ -7,6 +7,7 @@ import Logger from '../../../app/utils/logger';
 import { expect, sinon } from '../../utils/testUtils';
 
 const express = require('express');
+const proxyquire = require('proxyquire').noCallThru();
 
 describe('Home Office Details Controller', function () {
   let sandbox: sinon.SinonSandbox;
@@ -86,6 +87,21 @@ describe('Home Office Details Controller', function () {
   });
 
   describe('getNamesPage', () => {
+    let getNamePage;
+    beforeEach(() => {
+      const configStub = {
+        get: sinon.stub()
+            .withArgs('features.homeOfficeValidationEnabled')
+            .returns(false)
+      };
+      const homeOfficeDetailsController = proxyquire('../../../app/controllers/appeal-application/home-office-details', { config: configStub });
+      getNamePage = homeOfficeDetailsController.getNamePage;
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
     it('should render personal-details/name.njk', function () {
       getNamePage(req as Request, res as Response, next);
       expect(renderStub.calledOnceWith('appeal-application/personal-details/name.njk')).to.equal(true);
@@ -129,6 +145,24 @@ describe('Home Office Details Controller', function () {
           homeOfficeValidationEnabled: false
         }
       );
+    });
+
+    it('should render personal-details/name.njk with homeOfficeValidationEnabled set to true', function () {
+      const configStub = {
+        get: sinon.stub()
+            .withArgs('features.homeOfficeValidationEnabled')
+            .returns(true)
+      };
+      const homeOfficeDetailsController = proxyquire('../../../app/controllers/appeal-application/home-office-details', { config: configStub });
+      const getNamePage = homeOfficeDetailsController.getNamePage;
+
+      req.session.appeal.application.personalDetails = { givenNames: 'givenName', familyName: 'familyName', dob: null };
+      getNamePage(req as Request, res as Response, next);
+      expect(renderStub).to.be.calledOnceWith('appeal-application/personal-details/name.njk', {
+        personalDetails: req.session.appeal.application.personalDetails,
+        previousPage: paths.appealStarted.details,
+        homeOfficeValidationEnabled: true,
+      });
     });
   });
 

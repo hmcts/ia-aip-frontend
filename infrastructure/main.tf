d06b72940bd03b19e5e314439b0965ca502853db
kleog <kleogeorgiou@hotmail.com>
2026-07-22 11:48:39 +0100

    DIAC-2587 - updating main.tf to include new managed redis changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -47,3 +47,39 @@ resource "azurerm_key_vault_secret" "redis_connection_string" {
   value        = "redis://${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
   key_vault_id = "${data.azurerm_key_vault.ia_key_vault.id}"
 }
+
+module "redis_cache_managed_redis" {
+  # foreach conditional allows selective deployment to desired environments
+  for_each = toset(contains(["sandbox", "aat", "ithc", "perftest"], var.env) ? [var.env] : [])
+  source   = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
+
+  product     = var.product
+  component   = var.component       # NEW:        HMCTS component name — used to form the resource name |
+  env         = var.env
+  location    = var.location
+  common_tags = var.common_tags
+
+  # Performance:
+  sku_name = "Balanced_B0"          # NEW:        Be cautious, it's very expensive as usual
+
+  # Networking:
+  public_network_access   = "Disabled"
+  create_private_endpoint = true
+  subnet_id               = data.azurerm_subnet.redis_private_endpoint.id
+  private_dns_zone_ids    = ["/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redis.azure.net"]
+
+  access_keys_authentication_enabled = true
+
+  # Backup (persistence) options:
+  persistence_rdb_backup_frequency   = "6h"
+  # other available options (https://learn.microsoft.com/en-gb/azure/redis/how-to-persistence`):
+  ## persistence_aof_backup_frequency
+  ## geo_replication_group_name
+}
+
+# Create a new secret for the new managed redis instance, this allows for ease of rollback via Flux
+resource "azurerm_key_vault_secret" "managed_redis_connection_string" {
+  name         = "managed-redis-connection-string"
+  value        = "rediss://:${urlencode(module.redis_cache_managed_redis.access_key)}@${module.redis_cache_managed_redis.host_name}:${module.redis_cache_managed_redis.redis_port}?tls=true"
+  key_vault_id = module.azurerm_key_vault.key_vault_id
+}

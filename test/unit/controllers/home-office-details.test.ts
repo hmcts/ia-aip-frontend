bff7fe3918d23ded214250e974ee1778fe63c53e
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-08-03 14:38:00 +0100

    fix build issues

--- test/unit/controllers/home-office-details.test.ts
+++ test/unit/controllers/home-office-details.test.ts
@@ -4,7 +4,6 @@ import moment from 'moment';
 import {
   getDateLetterReceived,
   getDateLetterSent,
-  getHomeOfficeDetails,
   postDateLetterReceived, postDateLetterSent,
   postHomeOfficeDetails,
   setupHomeOfficeDetailsController

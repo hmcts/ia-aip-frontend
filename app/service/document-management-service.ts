77f7d648f2028562e476d653ee53c49b210766d9
tiagreaney <tiagreaney@gmail.com>
2026-09-02 15:04:13 +0100

    update tests

--- app/service/document-management-service.ts
+++ app/service/document-management-service.ts
@@ -53,11 +53,13 @@ class DocumentManagementService {
    * @param documentUrl the document url to be inserted in the map
    * @param documentMap the document map array.
    */
-  public addToDocumentMapper(documentUrl: string, documentMap: DocumentMap[]) {
+  public addToDocumentMapper(documentUrl: string, documentName: string,
+                             documentMap: DocumentMap[]) {
     const documentId: string = uuid();
     documentMap.push({
       id: documentId,
-      url: documentUrl
+      url: documentUrl,
+      name: documentName
     });
 
     return documentId;
